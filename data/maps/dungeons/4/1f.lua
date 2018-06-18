b3b512a7c465761bfcd512fccd92aaebcca87018
Franck Jousseaume <franck.jousseaume@syllan.net>
2018-06-18 17:17:01 +0200

    dungeon 4

--- data/maps/dungeons/4/1f.lua
+++ data/maps/dungeons/4/1f.lua
@@ -13,46 +13,120 @@ local game = map:get_game()
 local is_small_boss_active = false
 local is_boss_active = false
 
-local door_manager = require("scripts/maps/door_manager")
-local treasure_manager = require("scripts/maps/treasure_manager")
-local switch_manager = require("scripts/maps/switch_manager")
-local enemy_manager = require("scripts/maps/enemy_manager")
-local separator_manager = require("scripts/maps/separator_manager")
-local owl_manager = require("scripts/maps/owl_manager")
+ local door_manager = require("scripts/maps/door_manager")
+ local treasure_manager = require("scripts/maps/treasure_manager")
+ local enemy_manager = require("scripts/maps/enemy_manager")
+ local separator_manager = require("scripts/maps/separator_manager")
+ local owl_manager = require("scripts/maps/owl_manager")
 
 function map:on_started()
 
  local hero = map:get_hero()
   -- Init music
   game:play_dungeon_music()
-
-
-
+  treasure_manager:disappear_pickable(map, "pickable_small_key_1")
+  enemy_manager:create_teletransporter_if_small_boss_dead(map, false)
+  treasure_manager:appear_heart_container_if_boss_dead(map)
 end
 
 function map:on_opening_transition_finished(destination)
 
+  map:set_doors_open("door_group_1_", true)
+  map:set_doors_open("door_group_2_", true)
+  map:set_doors_open("door_group_3_", true)
+  map:set_doors_open("door_group_6_", true)
+  map:set_doors_open("door_group_small_boss_", true)
   if destination == dungeon_4_1_B then
     game:start_dialog("maps.dungeons.4.welcome")
   end
 
 end
 
-
--- Enemies
-
-
 -- Treasures
 
+treasure_manager:appear_pickable_when_enemies_dead(map, "enemy_group_8_", "pickable_small_key_1", nil)
 
 -- Doors
 
-door_manager:open_when_enemies_dead(map,  "enemy_group_4",  "door_group_1")
-
--- Blocks
-
+door_manager:open_when_enemies_dead(map,  "enemy_group_4_",  "door_group_1_")
+door_manager:open_when_enemies_dead(map,  "enemy_group_2_",  "door_group_2_")
+door_manager:open_when_enemies_dead(map,  "enemy_group_1_",  "door_group_3_")
+door_manager:open_when_enemies_dead(map,  "enemy_group_23_",  "door_group_6_")
+door_manager:open_when_switch_activated(map, "switch_1", "door_group_4_")
 
 -- Sensors events
+
+function sensor_1:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_4_", "door_group_1_")
+
+end
+
+function sensor_2:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_2_", "door_group_2_")
+
+end
+
+function sensor_3:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_2_", "door_group_2_")
+
+end
+
+function sensor_4:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_4_", "door_group_1_")
+
+end
+
+function sensor_5:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_4_", "door_group_1_")
+
+end
+
+function sensor_6:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_4_", "door_group_1_")
+
+end
+
+function sensor_7:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map,  "enemy_group_1_",  "door_group_3_")
+
+end
+
+function sensor_8:on_activated()
+
+  if is_small_boss_active == false then
+    is_small_boss_active = true
+    enemy_manager:launch_small_boss_if_not_dead(map)
+  end
+
+end
+
+function sensor_9:on_activated()
+
+  if is_boss_active == false then
+    is_boss_active = true
+    enemy_manager:launch_boss_if_not_dead(map)
+  end
+
+end
+
+function sensor_10:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map,  "enemy_group_23_",  "door_group_6_")
+
+end
+
+function sensor_11:on_activated()
+
+  door_manager:close_if_enemies_not_dead(map,  "enemy_group_23_",  "door_group_6_")
+
+end
 
 
 separator_manager:manage_map(map)
