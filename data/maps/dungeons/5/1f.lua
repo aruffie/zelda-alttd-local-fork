01f01a307a64e3a71e7f73b9328081e2bcda44bc
Binbin <contact@zeldaforce.net>
2018-06-19 21:49:48 +0200

    Dungeon 5

--- data/maps/dungeons/5/1f.lua
+++ data/maps/dungeons/5/1f.lua
@@ -19,6 +19,7 @@ local switch_manager = require("scripts/maps/switch_manager")
 local enemy_manager = require("scripts/maps/enemy_manager")
 local separator_manager = require("scripts/maps/separator_manager")
 local owl_manager = require("scripts/maps/owl_manager")
+--require("scripts/multi_events")
 
 function map:on_started()
 
@@ -34,7 +35,7 @@ end
 function map:on_opening_transition_finished(destination)
 
   treasure_manager:disappear_pickable(map, "pickable_small_key_1")
-  map:set_doors_open("door_group_6", true)
+  map:set_doors_open("door_group_6_", true)
   if destination == dungeon_5_1_B then
     game:start_dialog("maps.dungeons.5.welcome")
   end
@@ -44,29 +45,67 @@ end
 
 -- Enemies
 
+function map:init_skeletons()
+
+  local skeleton_step = game:get_value("dungeon_5_skeleton_step")
+  if skeleton_step == nil then
+    skeleton_step = 1
+  end
+  for enemy in map:get_entities("skeleton_") do
+    enemy:set_enabled(false)
+  end
+  local enemy = map:get_entity("skeleton_" .. skeleton_step)
+  if enemy ~= nil then
+    enemy:set_enabled(true)
+  end
+
+end
+
+function skeleton_1:on_dead()
+  game:set_value("dungeon_5_skeleton_step", 2)
+  game:play_dungeon_music()
+end
+
+function skeleton_2:on_dead()
+  game:set_value("dungeon_5_skeleton_step", 3)
+  game:play_dungeon_music()
+end
+
+function skeleton_3:on_dead()
+  game:set_value("dungeon_5_skeleton_step", 4)
+  game:play_dungeon_music()
+end
+
+function skeleton_4:on_dead()
+  game:set_value("dungeon_5_skeleton_step", 5)
+  game:play_dungeon_music()
+end
+
 
 -- Treasures
-treasure_manager:appear_chest_when_enemies_dead(map, "enemy_group_10", "chest_beak_of_stone")
-treasure_manager:appear_pickable_when_blocks_moved(map, "block_group_1", "pickable_small_key_1") 
+treasure_manager:appear_chest_when_enemies_dead(map, "enemy_group_10_", "chest_beak_of_stone")
+treasure_manager:appear_pickable_when_blocks_moved(map, "block_group_1_", "pickable_small_key_1") 
 
 
 -- Doors
 
-door_manager:open_when_enemies_dead(map,  "enemy_group_5",  "door_group_1")
-door_manager:open_when_enemies_dead(map,  "enemy_group_4",  "door_group_1")
-door_manager:open_when_enemies_dead(map,  "enemy_group_9",  "door_group_2")
-door_manager:open_when_enemies_dead(map,  "enemy_group_11",  "door_group_3")
-door_manager:open_when_enemies_dead(map,  "enemy_group_20",  "door_group_5")
-door_manager:open_when_enemies_dead(map,  "enemy_group_22",  "door_group_5")
-door_manager:open_when_enemies_dead(map,  "enemy_group_24",  "door_group_6")
+door_manager:open_when_enemies_dead(map,  "enemy_group_5_",  "door_group_1_")
+door_manager:open_when_enemies_dead(map,  "enemy_group_4_",  "door_group_1_")
+door_manager:open_when_enemies_dead(map,  "enemy_group_9_",  "door_group_2_")
+door_manager:open_when_enemies_dead(map,  "enemy_group_11_",  "door_group_3_")
+door_manager:open_when_enemies_dead(map,  "enemy_group_20_",  "door_group_5_")
+door_manager:open_when_enemies_dead(map,  "enemy_group_22_",  "door_group_5_")
 -- Small boss Step 1
-door_manager:open_when_enemies_dead(map,  "enemy_group_12",  "door_group_3")
+door_manager:open_when_enemies_dead(map,  "skeleton_1",  "door_group_3_")
 
 -- Small boss Step 2
-door_manager:open_when_enemies_dead(map,  "enemy_group_18",  "door_group_4")
+door_manager:open_when_enemies_dead(map,  "skeleton_2",  "door_group_4_")
 
 -- Small boss Step 3
-door_manager:open_when_enemies_dead(map,  "enemy_group_23",  "door_group_5")
+door_manager:open_when_enemies_dead(map,  "skeleton_3",  "door_group_5_")
+
+-- Small boss Step 4
+door_manager:open_when_enemies_dead(map,  "skeleton_4",  "door_group_6_")
 
 -- Blocks
 
@@ -75,43 +114,71 @@ door_manager:open_when_enemies_dead(map,  "enemy_group_23",  "door_group_5")
 
 function sensor_1:on_activated()
 
-  door_manager:close_if_enemies_not_dead(map, "enemy_group_4", "door_group_1")
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_4_", "door_group_1_")
 
 end
 
 function sensor_2:on_activated()
 
-  door_manager:close_if_enemies_not_dead(map, "enemy_group_5", "door_group_1")
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_5_", "door_group_1_")
 
 end
 
 function sensor_3:on_activated()
 
-  door_manager:close_if_enemies_not_dead(map, "enemy_group_12", "door_group_3")
+  local skeleton_step = game:get_value("dungeon_5_skeleton_step")
+  if skeleton_step == nil then
+    skeleton_step = 1
+  end
+  if skeleton_step == 1 then
+    door_manager:close_if_enemies_not_dead(map, "skeleton_1", "door_group_3_")
+    sol.audio.play_music("maps/dungeons/small_boss")
+  end
 
 end
 
 function sensor_4:on_activated()
 
-  door_manager:close_if_enemies_not_dead(map, "enemy_group_18", "door_group_4")
+  local skeleton_step = game:get_value("dungeon_5_skeleton_step")
+  if skeleton_step == nil then
+    skeleton_step = 1
+  end
+  if skeleton_step == 2 then
+    door_manager:close_if_enemies_not_dead(map, "skeleton_2", "door_group_4_")
+    sol.audio.play_music("maps/dungeons/small_boss")
+  end
 
 end
 
 function sensor_5:on_activated()
 
-  door_manager:close_if_enemies_not_dead(map, "enemy_group_23", "door_group_5")
+  local skeleton_step = game:get_value("dungeon_5_skeleton_step")
+  if skeleton_step == nil then
+    skeleton_step = 1
+  end
+  if skeleton_step == 3 then
+    door_manager:close_if_enemies_not_dead(map, "skeleton_3", "door_group_5_")
+    sol.audio.play_music("maps/dungeons/small_boss")
+  end
 
 end
 
 function sensor_6:on_activated()
 
-  door_manager:close_if_enemies_not_dead(map, "enemy_group_22", "door_group_5")
+  door_manager:close_if_enemies_not_dead(map, "enemy_group_22_", "door_group_5_")
 
 end
 
 function sensor_7:on_activated()
 
-  door_manager:close_if_enemies_not_dead(map, "enemy_group_24", "door_group_6")
+  local skeleton_step = game:get_value("dungeon_5_skeleton_step")
+  if skeleton_step == nil then
+    skeleton_step = 1
+  end
+  if skeleton_step == 4 then
+    door_manager:close_if_enemies_not_dead(map, "skeleton_4", "door_group_6_")
+    sol.audio.play_music("maps/dungeons/small_boss")
+  end
 
 end
 
@@ -142,7 +209,7 @@ auto_separator_15:register_event("on_activating", function(separator, direction4
   block_group_2_1:reset()
   local x, y = hero:get_position()
   if direction4 == 0 then
-    map:close_doors("door_group_4")
+    map:close_doors("door_group_4_")
   end
 end)
 
@@ -151,7 +218,7 @@ auto_separator_16:register_event("on_activating", function(separator, direction4
   block_group_2_1:reset()
   local x, y = hero:get_position()
   if direction4 == 1 then
-    map:close_doors("door_group_4")
+    map:close_doors("door_group_4_")
   end
 end)
 
@@ -160,7 +227,7 @@ auto_separator_21:register_event("on_activating", function(separator, direction4
   block_group_2_1:reset()
   local x, y = hero:get_position()
   if direction4 == 3 then
-    map:close_doors("door_group_4")
+    map:close_doors("door_group_4_")
   end
 end)
 
