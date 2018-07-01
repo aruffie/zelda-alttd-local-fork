2743688517b9bd0c9f2007bd8deb12343bc57b94
JulioTanaka <nunien@live.fr>
2018-07-01 02:15:23 +0200

    Redesign et color correction : Fireball + helmasaur + zols_gels+ maskass Jt

--- data/enemies/helmasaur_masked.lua
+++ data/enemies/helmasaur_masked.lua
@@ -1,37 +1,50 @@
--- Lua script of enemy helmasaur_masked.
--- This script is executed every time an enemy with this model is created.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation for the full specification
--- of types, events and methods:
--- http://www.solarus-games.org/doc/latest
-
 local enemy = ...
-local game = enemy:get_game()
-local map = enemy:get_map()
-local hero = map:get_hero()
-local sprite
-local movement
 
--- Event called when the enemy is initialized.
-function enemy:on_created()
+-- Molblin: goes in a random direction.
 
-  -- Initialize the properties of your enemy here,
-  -- like the sprite, the life and the damage.
-  sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
-  enemy:set_life(1)
-  enemy:set_damage(1)
-end
+enemy:set_life(2)
+enemy:set_damage(1)
 
--- Event called when the enemy should start or restart its movements.
--- This is called for example after the enemy is created or after
--- it was hurt or immobilized.
+local sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
+
+-- The enemy was stopped for some reason and should restart.
 function enemy:on_restarted()
 
-  movement = sol.movement.create("target")
-  movement:set_target(hero)
-  movement:set_speed(48)
-  movement:start(enemy)
+  local m = sol.movement.create("straight")
+  m:set_speed(0)
+  m:start(self)
+  local direction4 = math.random(4) - 1
+  self:go(direction4)
+
 end
+
+function enemy:on_movement_finished(movement)
+
+  local direction4 = math.random(4) - 1
+  self:go(direction4)
+
+end
+
+function enemy:on_obstacle_reached(movement)
+
+  local direction4 = math.random(4) - 1
+  self:go(direction4)
+
+end
+
+-- Makes the enemy walk towards a direction.
+function enemy:go(direction4)
+
+  -- Set the sprite.
+  sprite:set_animation("walking")
+  sprite:set_direction(direction4)
+
+  -- Set the movement.
+  local m = self:get_movement()
+  local max_distance = 40 + math.random(120)
+  m:set_max_distance(max_distance)
+  m:set_smooth(true)
+  m:set_speed(40)
+  m:set_angle(direction4 * math.pi / 2)
+end
+
