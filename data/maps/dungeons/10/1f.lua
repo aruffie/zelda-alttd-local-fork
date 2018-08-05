d2dc0f5c9371de23e1d27361410247eb03dbaf9c
Thomas Noury <thomas.noury@is06.com>
2018-08-05 16:01:01 +0200

    Some work on dungeon 10 rooms

--- data/maps/dungeons/10/1f.lua
+++ data/maps/dungeons/10/1f.lua
@@ -9,18 +9,23 @@
 
 local map = ...
 local game = map:get_game()
+local door_manager = require("scripts/maps/door_manager")
+local enemy_manager = require("scripts/maps/enemy_manager")
+
+door_manager:open_when_enemies_dead(map,  "keese_1",  "door_keese_1")
+door_manager:open_when_enemies_dead(map,  "maskass_1",  "door_maskass_1")
 
 -- Event called at initialization time, as soon as this map becomes is loaded.
 function map:on_started()
   
-  map:set_doors_open("door_enemy_1")
+  map:set_doors_open("door_keese_1")
   map:set_doors_open("door_horse_puzzle")
 end
 
 -- Enemy room 1
 function sensor_enemy_room_1:on_activated()
 
-  map:close_doors("door_enemy_1")
+  map:close_doors("door_keese_1")
 end
 
 -- TEST ONLY
