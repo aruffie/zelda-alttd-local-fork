2ebe3694d131abea84e33d9acc30c62a3d7f00a2
Christopho <christopho@solarus-games.org>
2018-08-05 19:01:10 +0200

    Fix music fade transtions

--- data/scripts/music_manager.lua
+++ data/scripts/music_manager.lua
@@ -18,14 +18,33 @@ require("scripts/multi_events")
 local music_manager = {}
 
 local music_fade_timer
+local initial_volume = sol.audio.get_music_volume()
+local next_music
 
 function music_manager:play_music_fade(context, music)
 
+  if music_fade_timer == nil and
+    music == sol.audio.get_music() then
+    -- No fade in progress and no music change: nothing to do.
+    return
+  end
+
   if music_fade_timer ~= nil then
+    if music == next_music then
+    -- Fade in progress but already planning to put the wanted music:
+    -- nothing to do.
+      return
+    end
+    
+    -- Fade in progress but with another music target.
+    -- Cancel the first fade.
+    sol.audio.set_music_volume(initial_volume)
     music_fade_timer:stop()
   end
 
-  local initial_volume = sol.audio.get_music_volume()
+  local initial_music = sol.audio.get_music()
+  next_music = music
+  initial_volume = sol.audio.get_music_volume()
   local volume = initial_volume
   local step = initial_volume / 10
   local changed = false
@@ -33,11 +52,20 @@ function music_manager:play_music_fade(context, music)
   context:register_event("on_finished", function()
     -- Just in case the map is changed or the game is closed
     -- during the fade.
-    sol.audio.set_music_volume(initial_volume)
+    if music_fade_timer ~= nil and music_fade_timer:get_remaining_time() > 0 then
+      sol.audio.set_music_volume(initial_volume)
+    end
   end)
 
   music_fade_timer = sol.timer.start(context, 100, function()
+
     if not changed then
+      if sol.audio.get_music() ~= initial_music then
+        -- Another music was set in the meantime.
+        sol.audio.set_music_volume(initial_volume)
+        return false
+      end
+
       volume = math.max(0, volume - step)
       sol.audio.set_music_volume(volume)
       if volume <= 0 then
@@ -46,6 +74,13 @@ function music_manager:play_music_fade(context, music)
       end
       return true
     end
+
+    if sol.audio.get_music() ~= music then
+      -- Another music was set in the meantime.
+      sol.audio.set_music_volume(initial_volume)
+      return false
+    end
+
     volume = math.min(initial_volume, volume + step)
     sol.audio.set_music_volume(volume)
 
