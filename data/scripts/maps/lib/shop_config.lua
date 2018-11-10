8e7c9dcd32e8811e2b289c5138a6b5a2fd95a1be
Binbin <contact@zeldaforce.net>
2018-11-10 21:52:56 +0100

    Shop

--- data/scripts/maps/lib/shop_config.lua
+++ data/scripts/maps/lib/shop_config.lua
@@ -6,6 +6,8 @@ return {
     price = 200,
     quantity = 1,
     placeholder = 1,
+    variant = 1,
+    dialog_id = "shovel",
     activation_condition = function(map)
       local item_shovel = map:get_game():get_item("shovel")
       local variant_shovel = item_shovel:get_variant()
@@ -16,6 +18,8 @@ return {
     price = 10,
     quantity = 10,
     placeholder = 1,
+    variant = 1,
+    dialog_id = "bomb",
     activation_condition = function(map)
       local item_shovel = map:get_game():get_item("shovel")
       local variant_shovel = item_shovel:get_variant()
@@ -26,6 +30,8 @@ return {
     price = 10,
     quantity = 3,
     placeholder = 2,
+    variant = 1,
+    dialog_id = "heart",
     activation_condition = function(map)
       return true
     end
@@ -34,6 +40,8 @@ return {
     price = 50,
     quantity = 1,
     placeholder = 3,
+    variant = 1,
+    dialog_id = "shield",
     activation_condition = function(map)
       return true
     end
--- data/scripts/maps/shop_manager.lua
+++ data/scripts/maps/shop_manager.lua
@@ -6,13 +6,10 @@ function shop_manager:init(map)
   local game = map:get_game()
   local hero = map:get_hero()
   local shop_products = require("scripts/maps/lib/shop_config.lua")
-  --local font = language_manager:get_menu_font(id)
-  --local font_number = language_manager:get_menu_font(id)
-    -- We go through the list of products
+  -- We go through the list of products
   for name, params in pairs(shop_products) do
     -- If the quest condition is true, create the product.
     shop_manager:add_product(map, name, params)
-    
   end
   
   -- Events
@@ -27,7 +24,6 @@ function shop_manager:init(map)
         local x_hero, y_hero = hero:get_position()
         if math.abs(y_placeholder - y_hero) < 48 and hero:get_direction() == 1 then
           if shop_manager.product.name == k then
-            shop_manager.product.entity:remove()
             hero:unfreeze()
             shop_manager:add_product(map, shop_manager.product.name, shop_manager.product.params)
             shop_manager.product = nil
@@ -35,18 +31,18 @@ function shop_manager:init(map)
           end
         end
       end
+      local merchant = map:get_entity("merchant")
+      local merchant_invisible = map:get_entity("merchant_invisible")
+      if hero:get_distance(merchant) <=16  or hero:get_distance(merchant_invisible) <=16  then
+        local direction4 = merchant:get_direction4_to(hero)
+        merchant:get_sprite():set_direction(direction4)
+        shop_manager:buy_product(map)
+      end
       return true
-    elseif command == "action" and hero:get_state() == "carrying" then
-      --if hero:get_distance(merchant) <=16  or hero:get_distance(merchant_invisible) <=16  then
-      --  local direction4 = merchant:get_direction4_to(hero)
-      --  merchant:get_sprite():set_direction(direction4)
-      --  shop_manager:buy_product(map, map.shop_manager_product)
-      --end
     elseif (command == "up" or command == "down" or command == "left" or command == "right") and link_move == true then
       return true
     end
-
-end)
+  end)
 
 end
 
@@ -83,11 +79,9 @@ function shop_manager:add_product(map, name, params)
         product_lifting:get_sprite():set_animation("invisible")
         function product_lifting:on_lifting()
           local sprite = product_lifting:get_sprite()
-          print(product_lifting)
           shop_manager.product = {
             name = name,
-            params = params,
-            entity = product_lifting
+            params = params
           }
           product:remove()
           game:set_custom_command_effect("action", "none")
@@ -115,5 +109,49 @@ function shop_manager:add_product(map, name, params)
       end  
     end
  end
+ 
+ function shop_manager:buy_product(map)
+   
+  if shop_manager.product == nil then
+    return false
+  end
+  local game = map:get_game()
+  local hero = map:get_hero()
+  game:start_dialog("maps.houses.mabe_village.shop_2.product" .. "_" .. shop_manager.product.params.dialog_id, function(answer)
+    if answer == 1 then
+      local error = false
+      -- Hearts
+      if shop_manager.product.name == "heart" then
+        if game:get_life() == game:get_max_life() then
+          error = true
+        end
+      elseif shop_manager.product.name == "shield" then
+        local item = game:get_item("shield")
+        local variant = item:get_variant()
+        if variant > 0 then
+          error = true
+        end
+      end
+      if error then
+          game:start_dialog("maps.houses.mabe_village.shop_2.merchant_4")
+      else
+        local money = game:get_money()
+        if money >= shop_manager.product.params.price then
+          hero:freeze()
+          hero:unfreeze()
+          shop_manager.product.entity:remove()
+          game:remove_money(shop_manager.product.params.price)
+          local item = game:get_item(shop_manager.product.name)
+          item:set_variant(shop_manager.product.params.variant)
+          game:start_dialog("maps.houses.mabe_village.shop_2.merchant_6")
+          shop_manager.product = nil
+        else
+          game:start_dialog("maps.houses.mabe_village.shop_2.merchant_3")
+        end
+      end
+    end
+  end)
+
+ end  
 
 return shop_manager
