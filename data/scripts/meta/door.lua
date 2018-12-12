a856dc630f3879e12160b08a752d57ca4d8759c7
Binbin <contact@zeldaforce.net>
2018-12-12 21:05:36 +0100

    Corrections donjon 1

--- data/scripts/meta/door.lua
+++ data/scripts/meta/door.lua
@@ -4,14 +4,20 @@
 local door_meta = sol.main.get_metatable("door")
 
 -- Include scripts
+local sound_is_played = false
 local audio_manager = require("scripts/audio_manager")
 require("scripts/multi_events")
 
 door_meta:register_event("on_opened", function(door)
   
   local sprite = door:get_sprite()
-  if sprite:get_animation() == "opening" then
+  local game = door:get_game() 
+  if sprite:get_animation() == "opening" and sound_is_played == false then
+    sound_is_played = true
     audio_manager:play_sound("others/dungeon_door_open")
+    sol.timer.start(game, 50, function()
+      sound_is_played = false
+    end)
   end
       
 end)
@@ -19,6 +25,7 @@ end)
 door_meta:register_event("on_closed", function(door)
 
   local sprite = door:get_sprite()
+  print(sprite:get_animation() )
   if sprite:get_animation() == "closing" then
     audio_manager:play_sound("others/dungeon_door_slam")
   end
