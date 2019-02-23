137fa77c426ae19426ddf4d9d6b67d7879b1f276
phoenixii <phoenixii@hotmail.fr>
2019-02-24 00:05:32 +0100

    Moving platforms : Make on-map configuration simpler

--- data/entities/moving_platform.lua
+++ data/entities/moving_platform.lua
@@ -4,12 +4,11 @@ local game = entity:get_game()
 local hero = game:get_hero()
 --local movement
 local sprite
-local default_x = 64
-local default_y = 0
-local amplitude_x=default_x
-local amplitude_y=default_y
 local start_x, start_y
 local old_x, old_y
+local direction, distance
+local ax, ay
+local angle
 -- Include scripts
 --require("scripts/multi_events")
 
@@ -18,36 +17,39 @@ local old_x, old_y
 entity:register_event("on_created", function()
   start_x, start_y = entity:get_position()
   entity:set_traversable_by(false)
-  amplitude_x = entity:get_property("amplitude_x")
-  if amplitude_x == nil then
-    amplitude_x = default_x
+  direction = entity:get_property("direction")
+  if direction == nil then
+    direction = 0
   end
-  amplitude_y = entity:get_property("amplitude_y")
-  if amplitude_y == nil then
-    amplitude_y = default_y
+  distance = entity:get_property("distance")
+  if distance == nil then
+    distance= 0
   end
   start_time=sol.main.get_elapsed_time()
-  
-
+  angle=(direction)/4*math.pi
+  ax = math.cos(angle)*distance/2
+  ay = -math.sin(angle)*distance/2
+  --print (ax, ay, angle)
 end)
 
 
 function entity:on_update()
   local dt=sol.main.get_elapsed_time()-start_time
-  local a=math.sin(dt/2500)
-  local new_x = start_x + a*a*amplitude_x
-  local new_y = start_y + a*a*amplitude_y
+  local a=-math.cos(dt/1200)+1
+  print (a)
+  local new_x = start_x + a*ax
+  local new_y = start_y + a*ay
   entity:set_position(new_x,new_y)
  
   --Handle the collision with the hero and synchronize movements
   local x,y,w,h=entity:get_bounding_box()
-  print("PLATFORM XY", x,y)
+  --print("PLATFORM XY", x,y)
   local hx, hy, hw ,hh=hero:get_bounding_box()
-  print ("HERO XY", hx, hy)
-  print("HERO WH", hw, hh)
+  --print ("HERO XY", hx, hy)
+  --print("HERO WH", hw, hh)
 
   if hx<=x+w and hx+hw>=x and hy<=y+h and hy+hh>=y-1 then
-    print "TOUCH DOWN"
+    --print "TOUCH DOWN"
     local dx, dy = x-old_x, y-old_y
     local xx, yy = hero:get_position()
     if not hero:test_obstacles(dx, dy) then
