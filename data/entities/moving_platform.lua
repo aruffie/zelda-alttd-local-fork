ca72058e49d9023975c491f0e3b09e6a16811883
phoenixii <phoenixii@hotmail.fr>
2019-02-24 21:03:02 +0100

    Add weight activated platfoorm (WIP)

--- data/entities/moving_platform.lua
+++ data/entities/moving_platform.lua
@@ -34,7 +34,6 @@ entity:register_event("on_created", function()
   angle=(direction)/4*math.pi
   ax = math.cos(angle)*distance/2
   ay = -math.sin(angle)*distance/2
-  --print (ax, ay, angle)
 end)
 local function move_hero_with_me()
     local x,y=entity:get_bounding_box()
@@ -48,37 +47,26 @@ end
 function entity:on_update()
   local dt=sol.main.get_elapsed_time()-start_time
   local a=-math.cos(dt/1200)+1
-  print (a)
   local new_x = start_x + a*ax
   local new_y = start_y + a*ay
   entity:set_position(new_x,new_y)
  
   --Handle the collision with the hero and synchronize movements
   local x,y,w,h=entity:get_bounding_box()
-  --print("PLATFORM XY", x,y)
   local hx, hy, hw ,hh=hero:get_bounding_box()
-  --print ("HERO XY", hx, hy)
-  --print("HERO WH", hw, hh)
 
   if is_semisolid then
     if hy+hh<y+1 then
       entity:set_traversable_by("hero", false)
-      --print("YOU SHALL NOT PASS !")
       if hx<=x+w and hx+hw>=x and hy+hh==y then
-      --print "TOUCH DOWN"
-      move_hero_with_me()
-      --print ("HERO XY", xx+dx, yy+dy)
-      
+      move_hero_with_me()     
       end
     else
       entity:set_traversable_by("hero", true)
-      --print("THIS PLATFORM DOESN'T EXIST")
     end
   else
     if hx<=x+w and hx+hw>=x and hy<=y+h and hy+hh>=y-1 then
-      --print "TOUCH DOWN"
       move_hero_with_me()
-      --print ("HERO XY", xx+dx, yy+dy)
     end
   end
   old_x, old_y = x, y
