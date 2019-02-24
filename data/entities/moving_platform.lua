8d58e489eaa0de593b54825e7f281c7b1eca6e9a
phoenixii <phoenixii@hotmail.fr>
2019-02-24 10:26:35 +0100

    Moving pltatforms : Add semi-solidity effect

--- data/entities/moving_platform.lua
+++ data/entities/moving_platform.lua
@@ -9,6 +9,7 @@ local old_x, old_y
 local direction, distance
 local ax, ay
 local angle
+local is_semitransparent
 -- Include scripts
 --require("scripts/multi_events")
 
@@ -25,13 +26,24 @@ entity:register_event("on_created", function()
   if distance == nil then
     distance= 0
   end
+  is_semitransparent = entity:get_property("is_semitransparent")
+  if is_semitransparent == nil then
+    is_semitransparent = false
+  end
   start_time=sol.main.get_elapsed_time()
   angle=(direction)/4*math.pi
   ax = math.cos(angle)*distance/2
   ay = -math.sin(angle)*distance/2
   --print (ax, ay, angle)
 end)
-
+local function move_hero_with_me()
+    local x,y=entity:get_bounding_box()
+    local dx, dy = x-old_x, y-old_y
+    local xx, yy = hero:get_position()
+    if not hero:test_obstacles(dx, dy) then
+    hero:set_position(xx+dx, yy+dy)
+    end
+end
 
 function entity:on_update()
   local dt=sol.main.get_elapsed_time()-start_time
@@ -48,14 +60,26 @@ function entity:on_update()
   --print ("HERO XY", hx, hy)
   --print("HERO WH", hw, hh)
 
-  if hx<=x+w and hx+hw>=x and hy<=y+h and hy+hh>=y-1 then
-    --print "TOUCH DOWN"
-    local dx, dy = x-old_x, y-old_y
-    local xx, yy = hero:get_position()
-    if not hero:test_obstacles(dx, dy) then
-    hero:set_position(xx+dx, yy+dy)
+  if is_semitransparent then
+    if hy+hh<y+1 then
+      entity:set_traversable_by("hero", false)
+      print("YOU SHALL NOT PASS !")
+      if hx<=x+w and hx+hw>=x and hy+hh==y then
+      --print "TOUCH DOWN"
+      move_hero_with_me()
+      --print ("HERO XY", xx+dx, yy+dy)
+      
+      end
+    else
+      entity:set_traversable_by("hero", true)
+      print("THIS PLATFORM DOESN'T EXIST")
     end
-    --print ("HERO XY", xx+dx, yy+dy)
+  else
+    if hx<=x+w and hx+hw>=x and hy<=y+h and hy+hh>=y-1 then
+      --print "TOUCH DOWN"
+      move_hero_with_me()
+      --print ("HERO XY", xx+dx, yy+dy)
+    end
   end
   old_x, old_y = x, y
 end
