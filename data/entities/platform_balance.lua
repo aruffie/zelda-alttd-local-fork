fdc41c23aae613fe58f64c6a2b1c4eebcccd1da8
phoenixii <phoenixii@hotmail.fr>
2019-02-25 14:24:22 +0100

    Platforms :  Refactor weight-activated variables + some cleanup  Add twin platforms (WIP)

--- data/entities/platform_balance.lua
+++ data/entities/platform_balance.lua
@@ -1,42 +1,39 @@
 -- Variables
 local entity = ...
+
+local easings=require "scripts/automation/easing"
+
 local game = entity:get_game()
 local hero = game:get_hero()
-local sprite
-local movement
+entity.is_reverse=false
+local m
 local new_x, new_y
 local old_x, old_y
 local min_speed, max_speed = 0, 92
-
-local needs_carrying
+local speed
+local id, group --will be used to activate it's twin entity
 local w, h
 -- Include scripts
 --require("scripts/multi_events")
 
 -- Event called when the custom entity is initialized.
 entity:register_event("on_created", function()
-
+  local name=entity:get_name()
+  id = tonumber(name:sub(-1))
+  group = name:sub(1, -3)
   sprite = entity:get_sprite()
   w, h = entity:get_size()
   old_x, old_y=entity:get_bounding_box()
-
   entity:set_traversable_by(false)
-  needs_carrying = entity:get_property("needs_carrying") --Set whether the hero needs to be carrying a to make the entity move when stepping on it
-  if needs_carrying == nil then
-    needs_carrying = false
-  end
-  movement = sol.movement.create("straight")
-  movement:set_speed(min_speed)
-  movement:set_angle(3*math.pi/2)
-  movement:set_max_distance(0)
-  movement:start(entity, function()
-    print "DONE"
-  end)
-  
+  speed = min_speed
+  m = sol.movement.create("straight")
+  m:set_speed(speed)
+  m:set_angle(3*math.pi/2)
+  m:set_max_distance(0)
+  m:start(entity)
 end)
 
 local function move_hero_with_me()
-    --print "Let's move"
     local x,y=entity:get_bounding_box()
     local dx, dy = x-old_x, y-old_y
     local xx, yy = hero:get_position()
@@ -46,21 +43,27 @@ local function move_hero_with_me()
 end
 
 function entity:on_update()
-  local speed=min_speed
-  local anim="moving"
   local state = hero:get_state()
   local x,y=entity:get_bounding_box()
   local hx, hy, hw, hh=hero:get_bounding_box()
-  if hx+hw<=x+w and hx>=x and hy<=y+h-1 and hy+hh>=y-1 then
-    if (needs_carrying=="false") or (needs_carrying=="true" and hero:get_state() == "carrying") then
-      speed=max_speed
-    end
-    if needs_carrying=="true" then
-      anim="moving_pot"
-    end
+  if hx<x+w and hx+hw>x and hy<=y+h-1 and hy+hh>=y-1 then
+    entity:get_map():get_entity(group.."_"..(3-id)).is_reversed=true
+    is_on_platform = true
+  else
+    is_on_platform = false
+    entity:get_map():get_entity(group.."_"..(3-id)).is_reversed=false
   end
-  sprite:set_animation(anim)
-  movement:set_speed(speed)
+  if entity.is_reversed then
+    m:set_angle(math.pi/2)
+  else
+    m:set_angle(3*math.pi/2)
+  end
+  if entity.is_reversed or is_on_platform then
+    speed = math.min(speed + 1, max_speed)
+  else
+    speed = math.max(speed - 1, min_speed)
+  end
+  m:set_speed(speed)
   move_hero_with_me()
   old_x, old_y = entity:get_bounding_box()
 end
