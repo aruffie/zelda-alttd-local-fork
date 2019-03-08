925e8b348f863ce7b63a9f8b3b3bcceed215aa45
phoenixii <phoenixii@hotmail.fr>
2019-03-08 18:25:51 +0100

    Make ladder work as in the original game + fix unreactive sprite issues

--- data/scripts/sideview_manager.lua
+++ data/scripts/sideview_manager.lua
@@ -5,11 +5,9 @@ require("scripts/multi_events")
 local walking_speed = 88
 local climbing_speed= 44
 local timer
-local sprite
 local vspeed =0
 local gravity = 0.2
 local max_vspeed=2.5
-
 local movement
 local is_sideview = false
 
@@ -21,6 +19,23 @@ function map_meta:is_sideview(sideview)
   return is_sideview
 end
 
+local function is_xy_ladder(map, x,y, layer)
+  layer=layer or 0
+  return map:get_ground(x,y, layer)=="ladder"
+end
+
+local function is_ladder(map, dy, layer)
+  layer = layer or 0
+  return map:get_ground(x,y+dy, layer)=="ladder"
+end
+
+local function test_ladder(entity, offset)
+  offset = offset or 0
+  local map=entity:get_map()
+  local x,y,w,h = entity:get_bounding_box() 
+  return is_xy_ladder(map, x+w/2, y) or is_xy_ladder(map, x+w/2, y+h+offset)
+end
+
 local function apply_gravity(entity)
   --Apply gravity
   local vspeed = entity.vspeed or 0
@@ -30,7 +45,9 @@ local function apply_gravity(entity)
   local x,y=entity:get_position()
   local dy=0
   while dy<=vspeed do 
-    if entity:test_obstacles(0,dy) then --we just landed.
+    if entity:test_obstacles(0,dy) or 
+    entity:get_type()=="hero" and
+    (not test_ladder(entity, -1) and test_ladder(entity, dy)) then --we just landed.
       vspeed = 0
       --print("Ground hit. Last valid position:"..y+dy)
       break
@@ -52,8 +69,12 @@ local function update_entities(map)    -- Allow other entities to fall with the 
     local has_property = entity:get_property("has_gravity")
     if entity:get_type() == "pickable" or entity:get_type()=="carried_object" then
       is_affected = true
-    elseif entity:get_type()=="hero" and not entity.on_ladder then
-      is_affected = true
+    elseif entity:get_type()=="hero" then
+      if entity.on_ladder then
+        is_affected = false
+      else
+        is_affected = true
+      end
     else
       is_affected = false
     end
@@ -65,23 +86,31 @@ local function update_entities(map)    -- Allow other entities to fall with the 
 end
 
 local function update_movement(hero, speed, angle, state)
+  local sprite = hero:get_sprite()
   state = state and state or ""
   if state ~= "" then
     state = state.."_"
-  end 
+  end
+
+  if speed>0 then
+    new_animation=state.."walking"
+  else
+    new_animation=state.."stopped"
+  end
+  --print(new_animation)
+
   if movement:get_speed() ~= speed then
-    if speed>0 then
-      sprite:set_animation(state.."walking")
-    else
-      sprite:set_animation(state.."stopped")
-    end
     --print(sol.main.get_elapsed_time(), "set_speed", movement:get_speed(), " -> ", speed)
     movement:set_speed(speed) 
   end
+
   if movement:get_angle() ~= angle then
     --print(sol.main.get_elapsed_time(), "set_angle", movement:get_angle(), " -> ", angle)
     movement:set_angle(angle) 
     sprite:set_direction(math.floor(angle*2/math.pi))
+  end
+  if new_animation ~= sprite:get_animation() then
+    sprite:set_animation(new_animation)
   end
 end
 
@@ -93,37 +122,35 @@ local function update_hero(hero, game)
 
   --print "LOOP"
   local map = game:get_map()
-  local function is_ladder(dy)
-    return map:get_ground(x,y+dy, layer)=="ladder"
-  end
+
+
   local speed = 0
   local angle = movement:get_angle()
   local dx, dy
+  local animation=""
   --print "COMMAND ?"
-  if is_ladder(- 4) or is_ladder(3) then
-    if command("up") or command("down") then
-      if command("up") and (not command("down")) and is_ladder(2) then
-        hero:get_sprite():set_direction(1)
-        --print "RIGHT"
-        angle=math.pi/2
-        speed=climbing_speed
-        hero.on_ladder = true
 
-      elseif command("down") and (not command("up")) then
-        hero:get_sprite():set_direction(3)
-        ---print "LEFT"
-        angle=1.5*math.pi
-        speed=climbing_speed
-      end
+  if command("up") and (not command("down")) then
+    --print "RIGHT"
+    angle=math.pi/2
+    if test_ladder(hero, -1) then
+      hero.on_ladder = true
+      speed=climbing_speed
     end
-    if hero:get_ground_below()=="ladder" then
-      hero.on_ladder = true 
-    else
-      hero.on_ladder = false
+  elseif command("down") and (not command("up")) then
+    ---print "LEFT"
+    angle=1.5*math.pi
+    if test_ladder(hero) then
+      hero.on_ladder = true
+      speed=climbing_speed
     end
-  else
+  end
+
+  if not test_ladder(hero) then
     hero.on_ladder = false
   end
+
+
   if command("right") and not command("left") then
     --print "RIGHT"
     speed=walking_speed
@@ -133,8 +160,9 @@ local function update_hero(hero, game)
     angle=math.pi
     speed=walking_speed
   end
+
 --print ("Movement speed:"..movement:get_speed()..", New speed:"..speed)
-  if hero.on_ladder then
+  if hero.on_ladder and test_ladder(hero, -1) then
     animation = "climbing"
   elseif hero:get_state() == "carrying" then
     animation = "carrying"    
@@ -171,7 +199,6 @@ hero_meta:register_event("on_state_changed", function(hero, state)
 
     local map = hero:get_map()
     if is_sideview then
-      sprite=hero:get_sprite()
       if state == "free" or state == "carrying" then
         movement=sol.movement.create("straight")
         movement:set_angle(-1)
