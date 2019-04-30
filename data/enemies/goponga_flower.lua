7b5fd83a5650e70fcb1a09d2eed3e6d1319e290d
Benjamin Schweitzer <contact@zeldaforce.net>
2019-04-30 13:58:02 +0200

    Sounds

--- data/enemies/goponga_flower.lua
+++ data/enemies/goponga_flower.lua
@@ -7,6 +7,10 @@ local game = enemy:get_game()
 local map = enemy:get_map()
 local hero = map:get_hero()
 local sprite
+local hero_is_bounce = false
+
+-- Include scripts
+local audio_manager = require("scripts/audio_manager")
 
 -- The enemy appears: set its properties.
 function enemy:on_created()
@@ -15,13 +19,21 @@ function enemy:on_created()
   enemy:set_traversable(false)
   enemy:set_life(1)
   enemy:set_damage(4)
-  enemy:set_attack_consequence("sword", function()
-      sprite:set_animation("bounce", function()
+  enemy:set_push_hero_on_sword(true)
+
+  enemy:set_attack_consequence("sworjjd", function()
+      if not hero_is_bounce then
+        hero_is_bounce = true
+        audio_manager:play_sound("hero/bounce")
+        sprite:set_animation("bounce", function()
+          hero_is_bounce = false
           sprite:set_animation("walking")
-      end)
+        end)
+      end
   end)
   enemy:set_hookshot_reaction(1)
   enemy:set_attack_consequence("boomerang", 1)
   enemy:set_attacking_collision_mode("touching")
+  enemy:set_push_hero_on_sword(true)
 
 end
