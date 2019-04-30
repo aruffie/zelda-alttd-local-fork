11a1984b529540b423c75d81684d923a9735723b
Benjamin Schweitzer <contact@zeldaforce.net>
2019-04-30 06:16:33 +0200

    Corrections sons + mapping

--- data/enemies/goponga_flower.lua
+++ data/enemies/goponga_flower.lua
@@ -7,7 +7,6 @@ local game = enemy:get_game()
 local map = enemy:get_map()
 local hero = map:get_hero()
 local sprite
-local movement
 
 -- The enemy appears: set its properties.
 function enemy:on_created()
@@ -15,8 +14,14 @@ function enemy:on_created()
   sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
   enemy:set_traversable(false)
   enemy:set_life(1)
-  enemy:set_damage(1)
-  enemy:set_invincible(true)
+  enemy:set_damage(4)
+  enemy:set_attack_consequence("sword", function()
+      sprite:set_animation("bounce", function()
+          sprite:set_animation("walking")
+      end)
+  end)
+  enemy:set_hookshot_reaction(1)
+  enemy:set_attack_consequence("boomerang", 1)
   enemy:set_attacking_collision_mode("touching")
 
 end
