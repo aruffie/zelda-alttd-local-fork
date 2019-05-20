4ceddd271ea2640473cace3102511c08d8455dbe
phoenixii <phoenixii@hotmail.fr>
2019-05-20 14:47:36 +0200

    Add documentation + cleanup for moving platforms

--- data/entities/platform_moving.lua
+++ data/entities/platform_moving.lua
@@ -1,3 +1,15 @@
+--[[
+  Generic moving platform, which oscillates back and forth.
+  It can be made semi-solid (you can go through while you are under it, and you can walk on it as soon as you are above it)
+  To customize it, use the following custom properties :
+    direction (0-7): the direction to follow from the starting position. Defaults to 0 (right)
+    distance: the mawimum distance in pisels to go from the starting position. Defaults to 0 (no movement)
+    cycle_duration: the back-and-forth duration. defaults to 4000 ms
+    is_semisolid: Whether it should be semi-solid. Dafaults to false
+    
+  Note: The way the semi-solidity is handled makes it incompatible with having multiples heroes.
+--]]
+
 -- Variables
 local entity = ...
 local game = entity:get_game()
@@ -11,9 +23,11 @@ local direction, distance, duration
 local ax, ay
 local angle
 local is_semisolid
+local is_solidified = true
 local elapsed_time
+
 -- Include scripts
---require("scripts/multi_events")
+require("scripts/multi_events")
 
 -- Event called when the custom entity is initialized.
 --function entity:on_created()
@@ -33,14 +47,8 @@ entity:register_event("on_created", function()
     if duration == nil then
       duration= 4000
     end
-    local semisolid = entity:get_property("is_semisolid")
-    if semisolid == nil then
-      is_semisolid = false
-    elseif semisolid == "true" then
-      is_semisolid=true
-    else
-      is_semisolid=false
-    end
+        
+    is_semisolid = entity:get_property("is_semisolid") == "true"
 
     elapsed_time=0
     angle=(direction)/4*math.pi
@@ -48,7 +56,9 @@ entity:register_event("on_created", function()
     ay = -math.sin(angle)*distance/2
   end)
 
+--This function makes the given entity maintain it's relative position to the platform
 local function move_entity_with_me(other)
+
   local x,y=entity:get_bounding_box()
   local dx, dy = x-old_x, y-old_y
   local xx, yy = other:get_position()
@@ -57,44 +67,51 @@ local function move_entity_with_me(other)
 
 end
 
-local function update_entity_position(other)
-  local x,y,w,h=entity:get_bounding_box()
-  local xx, yy, ww ,hh = other:get_bounding_box()
-  if is_semisolid then
-    if yy+hh<=y+1 then
-      entity:set_traversable_by("hero", false)
-      if xx<=x+w and xx+ww>=x and (yy+hh>=y-1 and yy+hh <= y+1) then
-        move_entity_with_me(other)
-      end
-    else
-      entity:set_traversable_by("hero", true)
-    end
-  else
-    if xx<=x+w and xx+ww>=x and yy<=y+h and yy+hh>=y-1 then
-      move_entity_with_me(other)
-    end
-  end
-  old_x, old_y = x, y
-end
 
 function entity:on_position_changed()
 
   local x,y,w,h = entity:get_bounding_box()
---  local i=0
 
-  for e in map:get_entities_in_rectangle(x-16, y-16, w+32, h+32) do
+  for other in map:get_entities_in_rectangle(x-16, y-16, w+32, h+32) do
 
-    if e ~= entity then
-      local e_type = e:get_type()
+    if other ~= entity then
+      local e_type = other:get_type()
 --      print(e_type)
       if e_type == "hero" or e_type == "npc" or e_type == "enemy" or e_type == "pickable" then
-        update_entity_position(e)
+        
+        --Update entity position start
+        local other_x, other_y, other_w , other_h = other:get_bounding_box()
+        
+        if is_semisolid then
+          if other_y+other_h <= y+1 then
+            if e_type == "hero" and is_solidified == true then
+              print "ME SOLID NOW"
+              is_solidified = false
+              entity:set_traversable_by("hero", false)
+            end
+            if other_x <= x+w and other_x+other_w >= x and other_y+other_h >= y-1 then
+              move_entity_with_me(other)
+            end
+          else
+            if e_type == "hero" and is_solidified == false then
+              print "ME NON SOLID NOW"
+              is_solidified = true
+              entity:set_traversable_by("hero", true)
+            end
+          end
+        else
+          if other_x<=x+w and other_x+other_w>=x and other_y<=y+h and other_y+other_h>=y-1 then
+            move_entity_with_me(other)
+          end
+        end
+
+        --update entity position end
+        
       end
     end
---    i=i+1
   end
-  print ("Job's done for" ..(entity:get_name() or "<some entity>")) 
-  -- print ("# of entities found by "..(entity:get_name() or "<some entity>")..": "..i) 
+  old_x, old_y = x, y
+-- print ("Job's done for" ..(entity:get_name() or "<some entity>")) 
 end
 
 function entity:on_update()
