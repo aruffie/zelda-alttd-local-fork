f50030be0783e78387a2788c329c73da4a3c0473
phoenixii <phoenixii@hotmail.fr>
2019-05-24 14:50:22 +0200

    Document "feather" item script

--- data/items/feather.lua
+++ data/items/feather.lua
@@ -1,17 +1,14 @@
--- Lua script of item feather.
--- This script is executed only once for the whole game.
+--[[
 
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
+  Lua script of item feather.
 
--- See the Solarus Lua API documentation for the full specification
--- of types, events and methods:
--- http://www.solarus-games.org/doc/latest
-
+  This newer version uses plainly the new global command overrides as it depends on not triggering the "item" state
+  The reason is that it would end any custon jumping state, with bad consequences, such as falling into a pit while mid-air
+  
+--]]
 local hero_meta = sol.main.get_metatable("hero")
 local item = ...
 local game = item:get_game()
---local hero = game:get_hero()
 
 -- Include scripts
 local audio_manager = require("scripts/audio_manager")
@@ -25,84 +22,37 @@ function item:on_started()
   item:set_savegame_variable("possession_feather")
   item:set_sound_when_brandished(nil)
   item:set_assignable(true)
-  -- Initialize the properties of your item here,
-  -- like whether it can be saved, whether it has an amount
-  -- and whether it can be assigned.
 end
 
 local game_meta = sol.main.get_metatable("game")
 
-
-game_meta:register_event("on_started", function(game)
-
---    game:register_event("on_command_pressed", function(game, command)
---        --print ("command ? > "..command)
---        local item_1=game:get_item_assigned(1)
---        local item_2=game:get_item_assigned(2)
---        if command=="item_1" and item_1 and item_1:get_name()=="feather"
---        or command=="item_2" and item_2 and item_2:get_name()=="feather" then
---          if not game:is_paused() then
-
---            --print "manually jumping"
---            --  print "FEATHER TIME"
---            local hero = game:get_hero()
---            local map = game:get_map()
---            if hero.is_jumping~=true then
---              if not map:is_sideview() then
---                -- print "ok"
---                local state = hero:get_state()
---                if state ~="falling" then
---                  if state == "sword swinging" or state =="sword loading" or state=="custom" and hero:get_state_object():get_description() == "flying_sword" then 
---                    hero:start_flying_attack()
---                  elseif state=="custom" and hero:get_state_object():get_description()=="running" then 
---                    jm.start(hero)
---                  else
---                    hero:start_jumping()
---                  end
---                end
---              else
-----      print "SIDEVIEW JUMP requested "
---                local vspeed = hero.vspeed or 0
---                if vspeed == 0 or map:get_ground(hero:get_position())=="deep_water" then
-----        print "validated, now jump :"
---                  audio_manager:play_sound("hero/jump")
---                  sol.timer.start(10, function()
---                      hero.on_ladder = false
---                      hero.vspeed = -4
---                    end)
---                end
---              end
---            end
---            --Don"t propagate the input or else we will trigger the "item", which would ruin the purpose of the custom states 
---            return true
---          end
---        end
---      end)
-
-  end)
-
+--This function is automatically called when the item command is pressed. it is similar to item:on_using, without state changing 
 function item:start_using()
-  print "started item using new method"
 
-  --print "manually jumping"
-  --  print "FEATHER TIME"
   local hero = game:get_hero()
   local map = game:get_map()
   if hero.is_jumping~=true then
     if not map:is_sideview() then
-      -- print "ok"
+      
+      --in top view maps, we have to account for the terrain, so we need custom states.
       local state = hero:get_state()
-      if state ~="falling" then
+      
+      --Fun fact : before adding this check, it was possible to glitch through pits by repeatedly jumping while sinking in the hole
+      if state ~="falling" then 
+        
+        --Jump with sword pulled out
         if state == "sword swinging" or state =="sword loading" or state=="custom" and hero:get_state_object():get_description() == "jumping_sword" then 
           hero:start_flying_attack()
         elseif state=="custom" and hero:get_state_object():get_description()=="running" then 
+          --Run'n'jump! 
+          --Note : In Diarandor's version, it wound have required three seperate states: one for running,, one for jumping, AND one for run'n'jumping, now we can just use apply the jump effect to the running state. 
           jm.start(hero)
         else
           hero:start_jumping()
         end
       end
     else
---      print "SIDEVIEW JUMP requested "
+      --In side view maps, we don't have to care about the terrain and movement, which is already handled by the sideview manager, so all we have do to is apply a vertical impulsion to the hero.
       local vspeed = hero.vspeed or 0
       if vspeed == 0 or map:get_ground(hero:get_position())=="deep_water" then
 --        print "validated, now jump :"
@@ -116,8 +66,9 @@ function item:start_using()
   end
 end
 
+--Theorically, we cold entierely remove this event, but in the perspective of reusine this item in other projects, it is a perfect tool to check your configuration. 
 function item:on_using()
-  print "this message should not be displayed if i am correct"
+  print "this message should never appear. If it does, then check your dependancies")
 
   -- Define here what happens when using this item
   -- and call item:set_finished() to release the hero when you have finished.
