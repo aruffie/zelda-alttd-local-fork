cf9c440d17cd79dce5b14efa48a5e32d98eb8f52
Vlag <valentin.soudier@gmail.com>
2019-08-08 03:55:29 +0200

    Fix wizzrobe after enemies reorganisation

--- data/enemies/wizzrobe/wizzrobe.lua
+++ data/enemies/wizzrobe/wizzrobe.lua
@@ -75,7 +75,7 @@ function enemy:shoot()
   }
 
   local beam = enemy:create_enemy({
-    breed = "wizzrobe_beam",
+    breed = enemy:get_breed() .. "_beam",
     x = dxy[direction + 1][1],
     y = dxy[direction + 1][2],
   })
