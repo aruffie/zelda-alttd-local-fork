82de1fd894a466a8c4866fd67e6b01568e3bc233
Vlag <valentin.soudier@gmail.com>
2019-08-08 04:03:02 +0200

    Make face_lamp enemy id correspond to the sprite to see it in editor

--- data/enemies/face_lamp/face_lamp.lua
+++ data/enemies/face_lamp/face_lamp.lua
@@ -6,7 +6,7 @@ local enemy = ...
 local behavior = require("enemies/lib/fire_breathing_statue")
 
 local properties = {
-  sprite = "enemies/" .. enemy:get_breed() .. "/" .. enemy:get_breed(),
+  sprite = "enemies/" .. enemy:get_breed(),
   projectile_breed = "eyegore_statue/eyegore_statue_fireball",
   projectile_sound = "enemies/face_lamp",
 }
