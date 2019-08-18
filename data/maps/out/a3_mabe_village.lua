35f56d9a6f688f1aeba82e8ddad7150ebcfcc3bb
stdgregwar <gregoirehirt@gmail.com>
2019-08-18 07:08:45 +0200

    fishing start

--- data/maps/out/a3_mabe_village.lua
+++ data/maps/out/a3_mabe_village.lua
@@ -191,8 +191,10 @@ function map:talk_to_fishman()
       game:start_dialog("maps.out.mabe_village.fishman_2", function()
         fishman_sprite:set_animation("walking")
         fishman_sprite:set_direction(2)
+        map:get_hero():teleport("sideviews/mabe_village/sideview_1", "pond_side")
       end)
       --TODO - CODING FISHING GAME
+      
     else
       game:start_dialog("maps.out.mabe_village.fishman_3", function()
         fishman_sprite:set_animation("walking")
