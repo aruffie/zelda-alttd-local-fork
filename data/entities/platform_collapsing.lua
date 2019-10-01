6463b82efa2568d8941af655716ac1bb878910c0
phoenixii <phoenixii@hotmail.fr>
2019-10-01 23:41:35 +0200

    Collapsing platforms are now semi-solid

--- data/entities/platform_collapsing.lua
+++ data/entities/platform_collapsing.lua
@@ -18,6 +18,8 @@ local hero = game:get_hero()
 local sprite
 local movement
 local max_speed = 22
+local solidified=true
+local old_x, old_y
 
 local needs_carrying
 -- Include scripts
@@ -27,7 +29,6 @@ require("scripts/multi_events")
 entity:register_event("on_created", function()
 
     sprite = entity:get_sprite()
-
     entity:set_traversable_by(false)
 
     --Set whether the hero needs to be carrying a to make the entity move when stepping on it
@@ -42,36 +43,103 @@ entity:register_event("on_created", function()
     movement:set_angle(3*math.pi/2)
     movement:set_max_distance(0)
     movement:start(entity)
+    old_x, old_y=entity:get_position()
   end)
 
-
-entity:add_collision_test(
-
-  function(entity, other)
+sol.timer.start(entity, 10, function()
     local x, y, w, h = entity:get_bounding_box()
-    local hx, hy, hw, hh = other:get_bounding_box()
-    return other:get_type() == "hero" and hx+hw<=x+w and hx>=x and hy<=y+h-1 and hy+hh>=y-1
-  end, 
+    local ex, ey=entity:get_position()
+    local dx, dy = ex-old_x, ey-old_y
 
-  function(entity, other)
-    
-    local speed=0
-    local anim="moving"
-    local state = hero:get_state()
+    local found=false
+    for other in entity:get_map():get_entities("hero") do 
 
-    if needs_carrying=="false" or (needs_carrying=="true" and hero:get_state() == "carrying") then
-      speed=max_speed
+      local xx, yy = other:get_position()
+      local hx, hy, hw, hh = other:get_bounding_box()
+      if hx+hw<=x+w and hx>=x and hy<=y+h-1 and hy+hh>=y-1 then
+        found=true 
+      end
+
+      if hy+hh <= y+1 then
+        if solidified == true then
+--              print "ME SOLID NOW"
+          solidified = false
+          entity:set_traversable_by("hero", false)
+          if hx+hw<=x+w and hx>=x and hy<=y+h-1 and hy+hh>=y-1 then
+            other:set_position(xx+dx, yy+dy)
+          end
+        end
+
+
+      else
+        if solidified == false then
+--              print "ME NON SOLID NOW"
+          solidified = true
+          entity:set_traversable_by("hero", true)
+        end
+      end
     end
 
-    --Display the angry visage (or whathever is used to mark the carriable requirement
-    if needs_carrying=="true" then
-      anim="moving_pot"
-    end
+    if found then
 
-    sprite:set_animation(anim)
+      local speed=0
+      local anim="moving"
+      local state = hero:get_state()
 
-    --Update the moving speed
-    if speed ~= movement:get_speed() then
-      movement:set_speed(speed)
+      if needs_carrying=="false" or (needs_carrying=="true" and hero:get_state() == "carrying") then
+        speed=max_speed
+      end
+
+      --Display the angry visage (or whathever is used to mark the carriable requirement
+      if needs_carrying=="true" then
+        anim="moving_pot"
+      end
+
+      sprite:set_animation(anim)
+
+      --Update the moving speed
+      if speed ~= movement:get_speed() then
+        movement:set_speed(speed)
+      end
+    else
+      movement:set_speed(0)
+      sprite:set_animation("moving")
     end
+    old_x, old_y=entity:get_position()
+    return true
   end)
+
+--entity:add_collision_test(
+
+--  function(entity, other) --test
+--    local x, y, w, h = entity:get_bounding_box()
+--    local hx, hy, hw, hh = other:get_bounding_box()
+--    return other:get_type() == "hero" and hx+hw<=x+w and hx>=x and hy<=y+h-1 and hy+hh>=y-1
+--  end, 
+
+--  function(entity, other) --callback
+
+--    local speed=0
+--    local anim="moving"
+--    local state = hero:get_state()
+
+--    if needs_carrying=="false" or (needs_carrying=="true" and hero:get_state() == "carrying") then
+--      speed=max_speed
+--    end
+--    local x, y=entity:get_position()
+--    --Display the angry visage (or whathever is used to mark the carriable requirement
+--    if needs_carrying=="true" then
+--      anim="moving_pot"
+--    end
+--    local dx, dy = x-old_x, y-old_y
+--    local xx, yy = other:get_position()
+--    if not other:test_obstacles(0, dy) then
+--      other:set_position(xx+dx, yy+dy)
+--    end
+--    sprite:set_animation(anim)
+
+--    --Update the moving speed
+--    if speed ~= movement:get_speed() then
+--      movement:set_speed(speed)
+--    end
+--  end)
