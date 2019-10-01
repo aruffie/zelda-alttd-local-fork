e21e4b17f9f62ee6e7f898f3826e2dd0b33eaa2b
benschwe <benjamin.schweitzer@publicis.sapient.com>
2019-10-01 23:13:10 +0200

    Phone messages

--- data/scripts/maps/lib/phone_messages_config.lua
+++ data/scripts/maps/lib/phone_messages_config.lua
@@ -2,23 +2,37 @@
 -- Feel free to change these values.
 
 return {
-  message_2 = {
+  {
     message_key = 2,
     activation_condition = function(map)
       return
         map:get_game():get_value("first_phone_call")
     end
   },
-  message_3 = {
+  {
     message_key = 3,
     activation_condition = function(map)
       return map:get_game():get_value("main_quest_step") > 7
     end
-  }
-  message_4 = {
-    message_key = 3,
+  },
+  {
+    message_key = 4,
     activation_condition = function(map)
       return map:get_game():get_value("main_quest_step") > 9
+    end
+  },
+  {
+    message_key = 5,
+    activation_condition = function(map)
+      return map:get_game():get_value("main_quest_step") > 10
+    end
+  },
+  {
+    message_key = 6,
+    activation_condition = function(map)
+      local item = map:get_game():get_item("magnifying_lens")
+      local variant = item:get_variant()
+      return map:get_game():get_value("main_quest_step") > 10 and variant > 3
     end
   }
 }
--- data/scripts/maps/phone_manager.lua
+++ data/scripts/maps/phone_manager.lua
@@ -9,11 +9,10 @@ function phone_manager:talk(map)
   local messages = require("scripts/maps/lib/phone_messages_config")
   local message_key = 1
   -- We go through the list of companions
-  for key, params in pairs(messages) do
-    print(key)
+  for key, params in ipairs(messages) do
     if params.activation_condition ~= nil and params.activation_condition(map) then
       message_key = params.message_key
-      print(message_key)
+        print(params.message_key)
     end
   end
   phone_sprite:set_animation("calling")
