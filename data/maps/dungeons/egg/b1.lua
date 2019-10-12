28a781e352735841584a6140093a62261d8a79d4
benschwe <benjamin.schweitzer@publicis.sapient.com>
2019-10-12 22:30:45 +0200

    Corrections Oeuf sacré

--- data/maps/dungeons/egg/b1.lua
+++ data/maps/dungeons/egg/b1.lua
@@ -1,17 +1,38 @@
+-- Variables
 local map = ...
 local game = map:get_game()
 local hero=map:get_hero()
-local maze_paths=require("scripts/maps/lib/windfish_maze_config")
-local audio_manager=require("scripts/audio_manager")
 local boss_path_index
 local boss_path_step=1
 
-function map:on_started()
-  boss_path_index=game:get_value("windfish_maze_boss_path_index") 
+-- Include scripts
+require("scripts/multi_events")
+local audio_manager = require("scripts/audio_manager")
+local separator_manager = require("scripts/maps/separator_manager")
+local maze_paths = require("scripts/maps/lib/windfish_maze_config")
+
+-- Map events
+map:register_event("on_started", function()
+    
+  -- Music
+  map:init_music()
+  -- Separators
+  separator_manager:init(map)
+  -- Path
+  boss_path_index=game:get_value("windfish_maze_boss_path_index") or math.random(#maze_paths)
+  -- path generation should already been done unless we loaded an existing save created before this has been implemented
+  
+end)
+
+-- Initialize the music of the map
+function map:init_music()
+
+  audio_manager:play_music("74_wind_fish_egg")
 
 end
 
 local function check_boss_path_advancement(entity, direction)
+  
   print ("Testing path entry. Path ID="..boss_path_index..", path step="..boss_path_step)
   local expected=maze_paths[boss_path_index][boss_path_step]
   print ("Expected direction : "..expected..", got "..direction)
@@ -36,7 +57,7 @@ end
 
 for i = 0, 2 do
   for  entity in map:get_entities("maze_path_"..i) do
-    entity.direction=i
+    entity.direction = i
     function entity:on_activated()
       game:set_value("tp_ground", "traversable")
       check_boss_path_advancement(entity, entity.direction)
@@ -45,10 +66,14 @@ for i = 0, 2 do
 end
 
 function maze_victory:on_activated()
+  
   audio_manager:play_sound("misc/secret")
-  boss_path_step=1
+  boss_path_step = 1
+  
 end
 
 function reset_path:on_activated()
-  boss_path_step=1
+  
+  boss_path_step = 1
+  
 end
