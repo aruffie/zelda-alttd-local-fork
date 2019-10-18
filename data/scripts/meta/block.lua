a3aaec1326570bcdf18a1c71236e6ffdd94f34b2
benschwe <benjamin.schweitzer@publicis.sapient.com>
2019-10-18 13:57:47 +0200

    Corrections + blcok falling

--- data/scripts/meta/block.lua
+++ data/scripts/meta/block.lua
@@ -6,6 +6,32 @@ local block_meta = sol.main.get_metatable("block")
 -- Include scripts
 local audio_manager = require("scripts/audio_manager")
 
+function block_meta:on_removed()
+  
+  local game = self:get_game();
+  local map = game:get_map()
+  if self:get_ground_below()== 'hole' then
+    local x, y, layer = self:get_position()
+    local block_entity = map:create_custom_entity({
+      name = "block",
+      sprite = self:get_sprite():get_animation_set(),
+      x = x,
+      y = y,
+      width = 16,
+      height = 16,
+      layer = layer,
+      direction = 0
+    })
+    block_entity:set_can_traverse_ground("hole", true) 
+    local sprite = block_entity:get_sprite()
+    sprite:set_animation("falling")
+    function sprite:on_animation_finished(animation)
+      block_entity:remove()
+    end
+  end
+  
+end  
+
 function block_meta:on_moving()
     
   local x_start, y_start = self:get_position() 
