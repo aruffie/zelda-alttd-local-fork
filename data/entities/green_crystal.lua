2404322357e8fc876691fed7cca9841d73e1cc90
phoenixii <phoenixii@hotmail.fr>
2019-11-24 10:43:25 +0100

    Fix nil error in blocks on_moved event

--- data/entities/green_crystal.lua
+++ data/entities/green_crystal.lua
@@ -27,7 +27,7 @@ end)
 entity:add_collision_test("facing", function(crystal, other, crystal_sprite, other_sprite)
 
   if is_destroy == false and other:get_type() =="hero" and hero:get_state() == "custom" and hero:get_state_object():get_description()=="running"  then
-    audio_manager:play_sound("misc/bush_cut")
+    audio_manager:play_sound("misc/rock_shatter")
     sprite:set_animation('destroy')
     is_destroy = true
     entity:set_traversable_by("hero", true)
