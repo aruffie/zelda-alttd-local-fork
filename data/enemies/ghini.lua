c29345eb933d76d741024a579cb89e1b13fefd7d
Vlag <valentin.soudier@gmail.com>
2019-12-06 17:13:07 +0100

    Helmasaur

--- data/enemies/ghini.lua
+++ data/enemies/ghini.lua
@@ -29,7 +29,7 @@ function enemy:start_flying_movement()
   local camera_x, camera_y = camera:get_position()
   local camera_width, camera_height = camera:get_size()
 
-  -- Target the random point in the opposite quarter the enemy is moving to.
+  -- Target a random point.
   local limit_box = {x = camera_x + 64, y = camera_y + 64, width = camera_width - 128, height = camera_height - 128}
   local target_x = math.random(limit_box.x, limit_box.x + limit_box.width)
   local target_y = math.random(limit_box.y, limit_box.y + limit_box.height)
