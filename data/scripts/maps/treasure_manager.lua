175b20291b7965ee7a8a0af99df1eb896a735354
Vlag <valentin.soudier@gmail.com>
2019-12-19 03:49:34 +0100

    Take care of potential children when map puzzle are waiting for enemies dead

--- data/scripts/maps/treasure_manager.lua
+++ data/scripts/maps/treasure_manager.lua
@@ -17,8 +17,12 @@ function treasure_manager:appear_chest_when_enemies_dead(map, enemy_prefix, ches
     end
   end
 
+  -- Setup for each enemy that matches the prefix and their potential children.
   for enemy in map:get_entities(enemy_prefix) do
     enemy:register_event("on_dead", enemy_on_dead)
+    enemy:register_event("on_enemy_created", function(enemy, child)
+      enemy:register_event("on_dead", enemy_on_dead)
+    end)
   end
 
 end
@@ -83,8 +87,12 @@ function treasure_manager:appear_pickable_when_enemies_dead(map, enemy_prefix, p
     end
   end
 
+  -- Setup for each enemy that matches the prefix and their potential children.
   for enemy in map:get_entities(enemy_prefix) do
     enemy:register_event("on_dead", enemy_on_dead)
+    enemy:register_event("on_enemy_created", function(enemy, child)
+      enemy:register_event("on_dead", enemy_on_dead)
+    end)
   end
 
 end
