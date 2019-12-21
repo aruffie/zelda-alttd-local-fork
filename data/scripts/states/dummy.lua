a3dde7476b1501de63329c206ec8e8ee0e89ed22
phoenixii <phoenixii@hotmail.fr>
2019-12-21 22:14:22 +0100

    Update test map and dummy state

--- data/scripts/states/dummy.lua
+++ data/scripts/states/dummy.lua
@@ -2,8 +2,7 @@
 
 local state = sol.state.create("dummy")
 
-state:set_can_use_stairs(false)
-state:set_can_traverse("stairs", false)
+state:set_can_use_stream(false)
 state:set_can_control_movement(true)
 state:set_can_control_direction(false)
 
@@ -18,9 +17,5 @@ end
 return function(e)
   print "Launching dummy state"
   e:start_state(state)
-  sol.timer.start(e:get_map(), 1000, function()
-      print "Testing dummy state"
-       state:set_can_use_stairs(true)
-    end)
 end
 
