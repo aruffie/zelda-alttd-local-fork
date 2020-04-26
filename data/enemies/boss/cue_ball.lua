0fccf3e64238e26cf4a177eaa5c4f9a6f5d50613
Vlag <valentin.soudier@gmail.com>
2020-04-27 00:43:51 +0200

    Cue Ball draft

--- data/enemies/boss/cue_ball.lua
+++ data/enemies/boss/cue_ball.lua
@@ -1,37 +1,101 @@
--- Lua script of enemy boss/cue_ball/cue_ball.
--- This script is executed every time an enemy with this model is created.
+----------------------------------
+--
+-- Cue Ball.
+--
+-- Charge to a direction 4 and turn right on obstacle reached.
+-- Spin around himself on hurt, and 
+--
+-- Methods : enemy:start_charging([direction4])
+--           enemy:start_spinning()
+--
+----------------------------------
 
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation for the full specification
--- of types, events and methods:
--- http://www.solarus-games.org/doc/latest
-
+-- Global variables
 local enemy = ...
+require("enemies/lib/common_actions").learn(enemy)
+
 local game = enemy:get_game()
 local map = enemy:get_map()
 local hero = map:get_hero()
-local sprite
-local movement
+local sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
+local hurt_frame_delay = sprite:get_frame_delay("hurt")
+local quarter = math.pi * 0.5
+local circle = math.pi * 2.0
 
--- Event called when the enemy is initialized.
-function enemy:on_created()
+-- Configuration variables
+local charging_angles = {0, quarter, 2.0 * quarter, 3.0 * quarter}
+local charging_speed = 120
+local waiting_duration = 500
+local spinning_minimum_duration = 500
+local spinning_maximum_duration = 1000
 
-  -- Initialize the properties of your enemy here,
-  -- like the sprite, the life and the damage.
-  sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
-  enemy:set_life(1)
-  enemy:set_damage(1)
+-- Hurt if the enemy angle to hero is not on the circle the enemy is looking at. 
+local function on_attack_received()
+
+  -- Don't hurt if a previous hurt animation is still running.
+  if sprite:get_animation() == "hurt" then
+    return
+  end
+
+  -- Manually hurt the enemy to not restart it automatically and immediatly start spinning.
+  local difference_angle = (sprite:get_direction() * quarter - enemy:get_angle(hero)) % circle
+  if difference_angle >= quarter and difference_angle <= 3 * quarter  then
+    enemy:set_life(enemy:get_life() - 1)
+    enemy:start_spinning()
+  else
+    -- Repulse one pixel to the back if not hurt.
+  end
 end
 
--- Event called when the enemy should start or restart its movements.
--- This is called for example after the enemy is created or after
--- it was hurt or immobilized.
-function enemy:on_restarted()
+-- Start the enemy movement.
+function enemy:start_charging(direction4)
 
-  movement = sol.movement.create("target")
-  movement:set_target(hero)
-  movement:set_speed(48)
-  movement:start(enemy)
+  direction4 = direction4 or sprite:get_direction()
+  enemy:start_straight_walking(charging_angles[direction4 + 1], charging_speed, nil, function()
+    sol.timer.start(enemy, waiting_duration, function()
+      enemy:start_charging((direction4 - 1) % 4)
+    end)
+  end)
 end
+
+-- Start spinning around himself for some time then start charging on the last direction of the spin.
+function enemy:start_spinning()
+
+  sprite:set_animation("hurt")
+  sol.timer.stop_all(enemy)
+  enemy:stop_movement()
+
+  local spinning_timer = sol.timer.start(enemy, hurt_frame_delay, function()
+    local frame = sprite:get_frame()
+    sprite:set_direction((sprite:get_direction() - 1) % 4)
+    sprite:set_frame(frame)
+    return true
+  end)
+  sol.timer.start(enemy, math.random(spinning_minimum_duration, spinning_maximum_duration), function()
+    spinning_timer:stop()
+    enemy:start_charging()
+  end)
+end
+
+-- Initialization.
+enemy:register_event("on_created", function(enemy)
+
+  enemy:set_life(8)
+  enemy:set_size(32, 32)
+  enemy:set_origin(16, 29)
+end)
+
+-- Restart settings.
+enemy:register_event("on_restarted", function(enemy)
+
+  -- Behavior for each items.
+  enemy:set_hero_weapons_reactions("ignored", {
+    sword = on_attack_received,
+    thrust = on_attack_received
+  })
+
+  -- States.
+  enemy:set_can_attack(true)
+  enemy:set_damage(4)
+  enemy:start_charging(0)
+end)
