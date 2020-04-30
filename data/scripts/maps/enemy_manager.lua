e67a81bfb7d500a922dc69d7dc4509f90d833721
Vlag <valentin.soudier@gmail.com>
2020-04-30 18:54:48 +0200

    Allow to have several small boss placeholders

--- data/scripts/maps/enemy_manager.lua
+++ data/scripts/maps/enemy_manager.lua
@@ -125,25 +125,31 @@ function enemy_manager:launch_small_boss_if_not_dead(map)
   local dungeon = game:get_dungeon_index()
   local dungeon_infos = game:get_dungeon()
   local savegame = "dungeon_" .. dungeon .. "_small_boss"
-  local placeholder = "placeholder_small_boss"
+
   if game:get_value(savegame) then
     return false
   end
-  local placeholder = map:get_entity(placeholder)
-  local x,y,layer = placeholder:get_position()
-  local game = map:get_game()
-  placeholder:set_enabled(false)
-  local enemy = map:create_enemy{
-    name = "enemy_small_boss",
-    breed = dungeon_infos["small_boss"]["breed"],
-    direction = 2,
-    x = x,
-    y = y,
-    layer = layer
-  }
-  enemy:register_event("on_dead", function()
-      enemy:launch_small_boss_dead()
+
+  -- May be several small bosses in the same room, loop on placeholders.
+  local enemy_prefix = "enemy_small_boss"
+  for placeholder in map:get_entities("placeholder_small_boss") do
+    local x, y, layer = placeholder:get_position()
+    placeholder:set_enabled(false)
+    local enemy = map:create_enemy{
+      name = enemy_prefix,
+      breed = dungeon_infos["small_boss"]["breed"],
+      direction = 2,
+      x = x,
+      y = y,
+      layer = layer
+    }
+    enemy:register_event("on_dead", function()
+      if not map:has_entities(enemy_prefix) then
+        enemy:launch_small_boss_dead()
+      end
     end)
+  end
+
   for tile in map:get_entities("tiles_small_boss_") do
     local layer = tile:get_property('start_layer')
     tile:set_layer(layer)
