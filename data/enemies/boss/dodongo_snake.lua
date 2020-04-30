ebad61b4274fba07ab8d40536df09a398a7d4048
Vlag <valentin.soudier@gmail.com>
2020-04-30 18:54:11 +0200

    Tweak Dodongo Snake again

--- data/enemies/boss/dodongo_snake.lua
+++ data/enemies/boss/dodongo_snake.lua
@@ -26,9 +26,9 @@ local quarter = math.pi * 0.5
 -- Configuration variables
 local walking_angles = {0, quarter, 2.0 * quarter, 3.0 * quarter}
 local walking_speed = 24
-local walking_minimum_distance = 16
-local walking_maximum_distance = 32
-local body_frame_lag = 14
+local walking_minimum_duration = 1000
+local walking_maximum_duration = 2000
+local body_frame_lag = 50
 local bomb_eating_duration = 1000
 
 local highest_frame_lag = body_frame_lag + 1 -- Avoid too much values in the last_positions table
@@ -46,16 +46,32 @@ local function get_possible_moving_angles(angles)
   return possible_angles
 end
 
+-- Update body sprite depending on previous head positions.
+local function update_body_sprite()
+
+  -- Save current position
+  local x, y, _ = enemy:get_position()
+  last_positions[frame_count] = {x = x, y = y}
+
+  -- Replace body sprite on a previous position.
+  local previous_position = last_positions[(frame_count - body_frame_lag) % highest_frame_lag] or last_positions[0]
+  body_sprite:set_xy(previous_position.x - x, previous_position.y - y)
+
+  frame_count = (frame_count + 1) % highest_frame_lag
+end
+
 -- Eat the given bomb.
 local function eat_bomb(bomb)
 
   enemy:stop_movement()
+  sol.timer.stop_all(enemy)
   bomb:remove()
   head_sprite:set_animation("bomb_eating", function()
     head_sprite:set_animation("immobilized")
 
     -- Eat and die if no more life
     if enemy:get_life() == 1 then
+      enemy:bring_sprite_to_front(body_sprite)
       body_sprite:set_animation("dying",function()
         local x, y = body_sprite:get_xy()
         enemy:start_brief_effect("entities/explosion_boss", nil, x, y)
@@ -84,8 +100,16 @@ end
 function enemy:start_walking()
 
   local angles = get_possible_moving_angles(walking_angles)
-  enemy:start_straight_walking(angles[math.random(#angles)], walking_speed, math.random(walking_minimum_distance, walking_maximum_distance), function()
-    enemy:start_walking()
+  enemy:start_straight_walking(angles[math.random(#angles)], walking_speed)
+
+  sol.timer.start(enemy, 10, function()
+    update_body_sprite()
+    return true
+  end)
+
+  -- Use time instead of distance for walking step to let the enemy go against a wall for some time.
+  sol.timer.start(enemy, math.random(walking_minimum_duration, walking_maximum_duration), function()
+    enemy:restart()
   end)
 end
 
@@ -98,25 +122,6 @@ enemy:register_event("on_update", function(enemy)
       eat_bomb(entity)
     end
   end
-end)
-
-
--- Update body sprite on position changed whatever the movement is.
-enemy:register_event("on_position_changed", function(enemy)
-
-  if not body_sprite then
-    return -- Workaround : Avoid this event to be called before enemy is actually created by the engine.
-  end
-
-  -- Save current position
-  local x, y, _ = enemy:get_position()
-  last_positions[frame_count] = {x = x, y = y}
-
-  -- Replace body sprite on a previous position.
-  local previous_position = last_positions[(frame_count - body_frame_lag) % highest_frame_lag] or last_positions[0]
-  body_sprite:set_xy(previous_position.x - x, previous_position.y - y)
-
-  frame_count = (frame_count + 1) % highest_frame_lag
 end)
 
 -- Initialization.
