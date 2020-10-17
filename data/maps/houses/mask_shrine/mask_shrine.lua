dc319492e95e2aba0e968a9d457d2c1e8d03ddb3
Vlag <valentin.soudier@gmail.com>
2020-10-17 17:50:36 +0200

    Armos Knight

--- data/maps/houses/mask_shrine/mask_shrine.lua
+++ data/maps/houses/mask_shrine/mask_shrine.lua
@@ -4,37 +4,40 @@ local game = map:get_game()
 -- Include scripts
 require("scripts/multi_events")
 local light_manager = require("scripts/maps/light_manager")
+local door_manager = require("scripts/maps/door_manager")
+local audio_manager = require("scripts/audio_manager")
 
 -- Event called at initialization time, as soon as this map becomes is loaded.
 map:register_event("on_started", function(map, destination)
 
   light_manager:init(map)
-  map:set_doors_open("door_boss_1")
-  --Boss
-  if game:get_value("southern_shrine_boss") then 
-    boss_sensor:set_enabled(false) 
-    map:set_doors_open("door_boss_2")
-  else boss:set_enabled(false) end
+  audio_manager:play_music("58_southern_shrine")
+
+  -- Doors
+  map:set_doors_open("door_boss_")
+
+  -- Boss
+  if game:get_value("armos_knight_killed") then 
+    armos_knight:remove()
+  else
+    door_manager:open_when_enemies_dead(map, "armos_knight", "door_boss_")
+    armos_knight:register_event("on_dead", function(armos_knight)
+      sol.audio.stop_music()
+      audio_manager:play_music("58_southern_shrine")
+    end)
+  end
 
 end)
 
---BOSS ACTIVED
+-- Sensors
 function boss_sensor:on_activated()
-    hero:freeze()
-    map:close_doors("door_boss")
-    sol.audio.stop_music()
-    sol.timer.start(1000,function()
-      hero:unfreeze()
-      boss:set_enabled(true)
-      audio_manager:play_music("small_boss")
-      boss_sensor:set_enabled(false)
-    end)
-end
---BOSS
-if boss ~= nil then
- function boss:on_dead()
-  audio_manager:play_sound("misc/secret1") 
-  audio_manager:play_music("southern_shrine")
-  map:open_doors("door_boss") 
- end
+
+  if game:get_value("armos_knight_killed") then
+    return
+  end
+
+  boss_sensor:set_enabled(false)
+  map:close_doors("door_boss_")
+  sol.audio.stop_music()
+  audio_manager:play_music("21_mini_boss_battle")
 end
