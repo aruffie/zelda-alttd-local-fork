c53fc7492474cc1d16d2deee9eed403c6a232b41
Vlag <valentin.soudier@gmail.com>
2020-10-31 00:29:03 +0100

    Smasher first draft

--- data/enemies/boss/smasher.lua
+++ data/enemies/boss/smasher.lua
@@ -22,9 +22,12 @@ local game = enemy:get_game()
 local map = enemy:get_map()
 local hero = map:get_hero()
 local sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
+local eighth = math.pi * 0.25
 local quarter = math.pi * 0.5
-local is_ball_carried_by_hero = false
-local ball
+local circle = math.pi * 2.0
+local is_ball_controlled_by_hero = false
+local is_ball_carried_by_enemy = false
+local ball, ball_sprite
 
 -- Configuration variables
 local ball_initial_offset_x = 80
@@ -32,14 +35,12 @@ local ball_initial_offset_y = 48
 local jumping_speed = 80
 local jumping_height = 6
 local jumping_duration = 200
-
--- Update the target direction depending on hero or ball position.
-local function update_direction()
-
-  local x, _, _ = enemy:get_position()
-  local hero_x, _, _ = hero:get_position()
-  sprite:set_direction(hero_x < x and 2 or 0)
-end
+local jumping_speed_while_carrying = 20
+local carrying_height = 30
+local jump_count_before_throwing = 3
+local throwing_speeds = {240, 40, 20}
+local throwing_durations = {600, 160, 70}
+local throwing_heights = {carrying_height, 4, 2}
 
 -- Create the ball and related events.
 local function create_ball()
@@ -55,12 +56,73 @@ local function create_ball()
     model = "ball",
     sprite = "entities/iron_ball"
   })
+  ball:set_traversable_by("enemy", true)
+  ball:set_can_traverse("enemy", true)
   ball:register_event("on_interaction", function(ball)
-    is_ball_carried_by_hero = true
+    is_ball_controlled_by_hero = true
   end)
   ball:register_event("on_finish_throw", function(ball)
-    is_ball_carried_by_hero = false
+    is_ball_controlled_by_hero = false
   end)
+  ball_sprite = ball:get_sprite()
+
+  -- Make the ball welded to the enemy on carried.
+  enemy:register_event("on_position_changed", function(enemy, x , y, layer)
+    if is_ball_carried_by_enemy then
+      ball:set_position(x, y, layer)
+    end
+  end)
+  sprite:register_event("set_xy", function(ball, x ,y)
+    if is_ball_carried_by_enemy then
+      ball_sprite:set_xy(x, y - carrying_height)
+    end
+  end)
+end
+
+-- Return the result of the xor operation.
+local function xor(a, b)
+  return (a or b) and not (a and b)
+end
+
+-- Return the normal angle of close obstacles as a multiple of pi/4, or nil if none.
+local function get_obstacles_normal_angle(entity)
+
+  local collisions = {
+    [0] = entity:test_obstacles(-1,  0),
+    [1] = entity:test_obstacles(-1,  1),
+    [2] = entity:test_obstacles( 0,  1),
+    [3] = entity:test_obstacles( 1,  1),
+    [4] = entity:test_obstacles( 1,  0),
+    [5] = entity:test_obstacles( 1, -1),
+    [6] = entity:test_obstacles( 0, -1),
+    [7] = entity:test_obstacles(-1, -1)
+  }
+
+  -- Return the normal angle for this direction if collision on the direction or the two surrounding ones, and no obstacle in the two next or obstacle in both.
+  local function check_normal_angle(direction8)
+    return ((collisions[direction8] or collisions[(direction8 - 1) % 8] and collisions[(direction8 + 1) % 8]) 
+        and not xor(collisions[(direction8 - 2) % 8], collisions[(direction8 + 2) % 8])
+        and direction8 * eighth)
+  end
+
+  -- Check for obstacles on each direction8 and return the normal angle if it is the correct one.
+  local normal_angle
+  for direction8 = 0, 7 do
+    normal_angle = normal_angle or check_normal_angle(direction8)
+  end
+
+  return normal_angle
+end
+
+-- Return the angle after bouncing against close obstacles towards the given angle, or nil if no obstacles.
+local function get_obstacles_bounce_angle(entity, angle)
+
+  local normal_angle = get_obstacles_normal_angle(entity)
+  if not normal_angle then
+    return
+  end
+
+  return (2.0 * normal_angle - angle + math.pi) % circle
 end
 
 -- Check if the custom death as to be started before triggering the built-in hurt behavior.
@@ -93,22 +155,131 @@ local function hurt(damage)
 end
 
 -- Start the enemy jumping movement.
-local function start_jumping()
+local function start_jumping(angle, speed, on_finished_callback)
 
-  --local movement = enemy:start_jumping(duration, jumping_height, angle, speed, function()
-
-  --end)
+  local movement = enemy:start_jumping(jumping_duration, jumping_height, angle, speed, function()
+    on_finished_callback()
+  end)
+  sprite:set_direction(angle > quarter and angle < 3.0 * quarter and 2 or 0)
 end
 
 -- Start throwing the ball to the hero.
-function enemy:start_throwing()
+local function start_throwing()
 
+  is_ball_carried_by_enemy = false
+
+  local time = 0
+  local function get_throwing_height(current_bounce)
+    time = time + 10
+    local progress = time / throwing_durations[current_bounce]
+    if current_bounce == 1 then
+      return 3 * throwing_heights[current_bounce] * (progress ^ 2 - progress) - (throwing_heights[current_bounce] * (1.0 - progress))
+    end
+    return 4 * throwing_heights[current_bounce] * (progress ^ 2 - progress)
+  end
+
+  sprite:set_animation("throwing", function()
+    sprite:set_animation("throwed")
+    local bounce_count = 1
+    local angle = ball:get_angle(hero)
+    local movement = sol.movement.create("straight")
+    movement:set_speed(throwing_speeds[bounce_count])
+    movement:set_angle(angle)
+    movement:set_smooth(false)
+    movement:start(ball)
+
+    -- Update the ball height each frames.
+    local throwing_timer = sol.timer.start(ball, 10, function()
+      local height = get_throwing_height(bounce_count)
+      if time <= throwing_durations[bounce_count] then
+        ball_sprite:set_xy(0, height)
+      else
+
+        -- Restart the enemy on the first bounce, and start three additionnal ones.
+        if bounce_count == 1 then
+          enemy:restart()
+          movement:set_speed(10)
+        elseif bounce_count == 3 then
+          movement:stop()
+          ball:set_weight(1)
+          ball:clear_collision_tests()
+          return
+        end
+        bounce_count = bounce_count + 1
+        movement:set_speed(throwing_speeds[bounce_count])
+        time = 0
+      end
+      return true
+    end)
+
+    -- Bonk on obstacle reached.
+    function movement:on_obstacle_reached()
+      movement:set_angle(get_obstacles_bounce_angle(ball, movement:get_angle()))
+    end
+  end)
 end
 
--- Remove the ball on dead.
-enemy:register_event("on_dead", function(enemy)
-  ball:remove()
-end)
+-- Start carrying the ball.
+local function start_carrying()
+
+  local x, y = enemy:get_position()
+  local direction = sprite:get_direction()
+  local lifting_trajectories = {
+  {{-20, 0}, {0, 0}, {6, -12},  {7, -12}, {7, -carrying_height + 24}},
+  {{20, 0}, {0, 0}, {-6, -12}, {-7, -12},  {-7, -carrying_height + 24}}}
+  local movement = sol.movement.create("pixel")
+  movement:set_trajectory(lifting_trajectories[direction == 2 and 1 or 2])
+  movement:set_ignore_obstacles(true)
+  movement:set_delay(100)
+
+  ball:set_position(x , y)
+  ball_sprite:set_xy(0, 0)
+  movement:start(ball_sprite)
+  sprite:set_animation("carrying")
+
+  -- Make three little jumps when carrying finished then start throwing the ball.
+  local function jump_before_throwing(jump_count)
+    start_jumping(enemy:get_angle(hero), jumping_speed_while_carrying, function()
+      if jump_count < jump_count_before_throwing then
+        jump_before_throwing(jump_count + 1)
+      else
+        start_throwing()
+      end
+    end)
+  end
+  function movement:on_finished()
+    is_ball_carried_by_enemy = true
+    jump_before_throwing(0)
+  end
+
+  -- Make the ball not carriable by the hero and hurt him on touched.
+  ball:set_weight(-1) -- Avoid the ball to be lifted by the hero.
+  ball:add_collision_test("sprite", function(ball, entity)
+    if entity:get_type() == "hero" and not entity:is_blinking() then
+      entity:start_hurt(ball, enemy:get_damage())
+    end
+  end)
+  ball:set_traversable_by("hero", true)
+  ball:set_can_traverse("hero", true)
+end
+
+-- Start the enemy panic jumping movement.
+local function start_panicking()
+
+  sprite:set_animation("panicking")
+  start_jumping(math.random() * math.pi, jumping_speed, function()
+    enemy:restart()
+  end)
+end
+
+-- Start the enemy movement to the ball.
+local function start_moving_to_ball()
+
+  sprite:set_animation("walking")
+  start_jumping(enemy:get_angle(ball), jumping_speed, function()
+    enemy:restart()
+  end)
+end
 
 -- Enable the ball on enabled.
 enemy:register_event("on_enabled", function(enemy)
@@ -127,9 +298,9 @@ enemy:register_event("on_created", function(enemy)
   enemy:set_size(48, 48)
   enemy:set_origin(24, 45)
   enemy:start_shadow("enemies/boss/armos_knight/shadow") -- TODO Create a specific shadow.
+  enemy:set_drawn_in_y_order(false) -- Display the legs and body part as a flat entity.
 
   create_ball()
-  update_direction()
 end)
 
 -- Restart settings.
@@ -140,5 +311,13 @@ enemy:register_event("on_restarted", function(enemy)
   enemy:set_can_attack(true)
   enemy:set_damage(4)
   enemy:set_pushed_back_when_hurt(false)
-  start_jumping()
+  if not is_ball_controlled_by_hero then
+    if not enemy:overlaps(ball, "touching") then
+      start_moving_to_ball()
+    else
+      start_carrying()
+    end
+  else
+    start_panicking()
+  end
 end)
--- data/entities/lib/carriable.lua
+++ data/entities/lib/carriable.lua
@@ -1,7 +1,8 @@
 ----------------------------------
 --
--- Undestructible destructible map entity, behaving the same way than build-in destructible except it bounces instead of breaking.
--- A hit happen when the entity reaches an obstacle or when the carriable sprite overlaps another entity sprite while the throw is still running.
+-- Undestructible destructible map entity, behaving the same way than build-in destructible except it bounces on obstacle reached instead of breaking.
+-- A hit may happen when the entity reaches an obstacle or when the carriable sprite overlaps another entity sprite while the throw is running.
+-- An entity can only be hit once in a throw, however the throw will still bonk on obstacles entites without triggering the hit behavior.
 -- 
 -- Methods : carriable:throw(direction)
 --
@@ -18,10 +19,6 @@
 --
 ----------------------------------
 
---TODO don't hurt an enemy twice
---TODO ensure get_overlapping_entities_on_obstacle_reached works correctly
---TODO remove is_obstacle()
-
 local carriable_behavior = {}
 local carrying_state = require("scripts/states/carrying.lua")
 
@@ -32,8 +29,9 @@ local default_properties = {
   bounce_heights = {nil, 4, 2}, -- Heights for each bounce. Nil means sprite position.
   bounce_sound = nil, -- Default id of the bouncing sound. Nil means no sound.
   respawn_delay = nil, -- Time before respawn when removed by bad grounds. Nil means no respawn.
-  shadow_sprite = nil, -- Sprite of the shadow. A default one is used if nil.
   slowdown_ratio = 0.5, -- Speed and distance decrease ratio at each obstacle hit.
+  is_bounding_box_collision_sensitive = true, -- Trigger a hit on bounding box collision.
+  is_sprite_collision_sensitive = true -- Trigger a hit on sprite collision.
 }
 
 function carriable_behavior.apply(carriable, properties)
@@ -42,6 +40,52 @@ function carriable_behavior.apply(carriable, properties)
   local map = carriable:get_map()
   local hero = map:get_hero()
   local sprite = carriable:get_sprite()
+  local shadow = nil
+
+  -- Add a shadow below the carriable as an sub entity to not interfere with a possible collision test from outside.
+  if not shadow then
+    local x, y, layer = carriable:get_position()
+    shadow = map:create_custom_entity({
+      direction = 0,
+      x = x,
+      y = y,
+      layer = layer,
+      width = 0,
+      height = 0,
+      sprite = "entities/shadows/shadow"
+    })
+    shadow:set_weight(-1)
+    shadow:set_traversable_by(true)
+    shadow:set_drawn_in_y_order(false) -- Display the shadow as a flat entity.
+    shadow:bring_to_back()
+
+    -- Make the shadow not visible on lifted and carried.
+    carriable:register_event("on_interaction", function(carriable)
+      shadow:set_visible(false)
+    end)
+    carriable:register_event("on_thrown", function(carriable, direction)
+      shadow:set_visible(true)
+    end)
+
+    -- Echo some of the carriable event on the shadow.
+    carriable:register_event("on_position_changed", function(carriable, x, y, layer)
+      shadow:set_position(x, y, layer)
+    end)
+    carriable:register_event("on_removed", function(carriable)
+      if shadow:exists() then
+        shadow:remove()
+      end
+    end)
+    carriable:register_event("on_enabled", function(carriable)
+      shadow:set_enabled()
+    end)
+    carriable:register_event("on_disabled", function(carriable)
+      shadow:set_enabled(false)
+    end)
+    carriable:register_event("set_visible", function(carriable, visible)
+      shadow:set_visible(visible)
+    end)
+  end
 
   -- Function to set the main sprite animation if it exists.
   local function set_animation_if_exists(animation)
@@ -73,34 +117,16 @@ function carriable_behavior.apply(carriable, properties)
     end)
   end
 
-  -- Return true if the parameter is an obstacle entity.
-  -- TODO Check for something like entity1:is_traversable_by(entity2) and remove this temp function
-  local function is_obstacle(entity)
-    local obstacle_entities = {"crystal", "custom_entity", "enemy"}
-    for _, entity_type in pairs(obstacle_entities) do
-      if entity:get_type() == entity_type then
+  -- Returns true if there is at least one obstacle in given entities.
+  local function is_obstacle_in(entities)
+    for _, entity in pairs(entities) do
+      -- Workaround: No fucking way to get traversable entities, hardcode ones that will have a triggered behavior or have the on_hit_by_carriable event defined...
+      local type = entity:get_type()
+      if type == "enemy" or type == "crystal" or entity.on_hit_by_carriable then
         return true
       end
     end
     return false
-  end
-
-  -- Simulate the movement that hasn't been commited and return a table with overlapping entities.
-  -- TODO Check for something like movement:on_obstacle_reached(entities) and remove this temp function
-  local function get_overlapping_entities_on_obstacle_reached(movement)
-    local overlapping_entities = {}
-    local speed = movement:get_speed()
-    local angle = movement:get_angle()
-    local movement_x = speed / 100 * math.cos(angle)
-    local movement_y = speed / 100 * math.sin(angle)
-    local x, y, width, height = carriable:get_max_bounding_box()
-    local entities = map:get_entities_in_rectangle(x + movement_x, y + movement_y, width, height)
-    for entity in entities do
-      if entity ~= carriable then
-        table.insert(overlapping_entities, entity)
-      end
-    end
-    return overlapping_entities
   end
 
   -- Throwing method, define behavior for the thrown carriable.
@@ -112,8 +138,9 @@ function carriable_behavior.apply(carriable, properties)
     local bounce_heights = properties.bounce_heights or default_properties.bounce_heights
     local bounce_sound = properties.bounce_sound or default_properties.bounce_sound
     local respawn_delay = properties.respawn_delay or default_properties.respawn_delay
-    local shadow_sprite = properties.shadow_sprite or default_properties.shadow_sprite
     local slowdown_ratio = properties.slowdown_ratio or default_properties.slowdown_ratio
+    local is_bounding_box_collision_sensitive = properties.is_bounding_box_collision_sensitive or default_properties.is_bounding_box_collision_sensitive
+    local is_sprite_collision_sensitive = properties.is_sprite_collision_sensitive or default_properties.is_sprite_collision_sensitive
 
     -- Initialize throwing state.
     local num_bounces = #bounce_distances
@@ -122,6 +149,7 @@ function carriable_behavior.apply(carriable, properties)
     local is_bounce_movement_starting = true -- True when the carriable is not moving, but about to.
     local dx, dy = math.cos(direction * math.pi / 2), -math.sin(direction * math.pi / 2)
     local _, hero_height = map:get_entity("hero"):get_size()
+    local unhittable_entities = {}
 
     carriable:set_direction(direction)
     carriable:set_traversable_by("hero", true)
@@ -130,30 +158,60 @@ function carriable_behavior.apply(carriable, properties)
     sprite:set_xy(0, -hero_height - 6)
     set_animation_if_exists("thrown")
 
-    -- Function to hurt an enemy vulnerable to thrown items.
-    local function hurt_if_vulnerable(entity)
-      if entity and entity:get_type() == "enemy" then
-      local reaction = entity:get_attack_consequence("thrown_item")
-        if reaction ~= "ignored" and reaction ~= "protected" then
-          entity:receive_attack_consequence("thrown_item", reaction)
-        end
-      end
-    end
-
     -- Callback function for bad ground bounce.
     -- Remove the carriable and respawn it after a delay if the property is set.
     local function on_bad_ground_bounce()
       local initial_properties = {
-          name = carriable:get_name(), model = carriable:get_model(), properties = carriable:get_properties(),
-          x = carriable.respawn_position.x, y = carriable.respawn_position.y, layer = carriable.respawn_position.layer, 
-          direction = carriable:get_direction(), sprite = sprite:get_animation_set(),
-          width = 16, height = 16}
+        name = carriable:get_name(), model = carriable:get_model(), properties = carriable:get_properties(),
+        x = carriable.respawn_position.x, y = carriable.respawn_position.y, layer = carriable.respawn_position.layer, 
+        direction = carriable:get_direction(), sprite = sprite:get_animation_set(),
+        width = 16, height = 16}
       carriable:remove()
       if respawn_delay then
-        sol.timer.start(respawn_delay, function()
+        sol.timer.start(map, respawn_delay, function()
           map:create_custom_entity(initial_properties)
         end)
       end
+    end
+
+    -- Return true if the entity is not already hit during this throw.
+    local function is_hittable(entity)
+      for _, unhittable_entity in pairs(unhittable_entities) do
+        if unhittable_entity == entity then
+          return false
+        end
+      end
+      return true
+    end
+
+    -- Simulate the movement that hasn't been commited and return a table with bounding box overlapping entities.
+    -- Workaround function to know what is obstacle entities reached during movement:on_obstacle_reached()
+    local function get_overlapping_entities_on_obstacle_reached(movement)
+      local overlapping_entities = {}
+      local speed = movement:get_speed()
+      local angle = movement:get_angle()
+      local movement_x = speed / 100 * math.cos(angle)
+      local movement_y = speed / 100 * math.sin(angle)
+      local x, y, width, height = carriable:get_max_bounding_box()
+      for entity in map:get_entities_in_rectangle(x + movement_x, y + movement_y, width, height) do
+        if entity ~= carriable and is_hittable(entity) then
+          table.insert(overlapping_entities, entity)
+        end
+      end
+      return overlapping_entities
+    end
+
+    -- Returns entites the have a sprite or bounding box collision with the carriable.
+    local function get_overlapping_entities()
+      local overlapping_entities = {}
+      for entity in map:get_entities_in_region(carriable) do
+        local is_bounding_box_collision = is_bounding_box_collision_sensitive and carriable:overlaps(entity)
+        local is_sprite_collision = is_sprite_collision_sensitive and carriable:overlaps(entity, "sprite")
+        if entity ~= carriable and is_hittable(entity) and (is_bounding_box_collision or is_sprite_collision) then
+          table.insert(overlapping_entities, entity)
+        end
+      end
+      return overlapping_entities
     end
 
     -- Reverse throwing direction and slow down all bounces including the current movement.
@@ -172,30 +230,30 @@ function carriable_behavior.apply(carriable, properties)
       end
     end
 
-    -- Callback function for collision test.
-    -- Call hit events and reverse the movement if needed.
-    local function carriable_on_collision(carriable, entity)
-      if entity and entity:is_enabled() and is_obstacle(entity) then
-        reverse_direction(slowdown_ratio)
-        hurt_if_vulnerable(entity)
-        call_hit_events(entity)
+    -- Trigger hit entities behavior.
+    local function hit(entities)
+      for _, entity in pairs(entities) do
+        if entity and entity:is_enabled() then
+          table.insert(unhittable_entities, entity) -- Avoid the entity being hit twice a throw.
+
+          if entity:get_type() == "enemy" then
+            local reaction = entity:get_attack_consequence("thrown_item")
+            if reaction ~= "ignored" and reaction ~= "protected" then
+              entity:receive_attack_consequence("thrown_item", reaction)
+            end
+          elseif entity:get_type() == "crystal" then
+            map:set_crystal_state(not map:get_crystal_state())
+          end
+
+          call_hit_events(entity)
+        end
       end
-    end
-
-    -- A hit may happen on sprite collision without reaching an obstacle when entities are not on the same row nor column.
-    carriable:add_collision_test("sprite", carriable_on_collision) -- TODO Seems buggy between custom entities
-
-    -- Create a sprite for the shadow.
-    if not shadow_sprite then
-      shadow_sprite = carriable:create_sprite("entities/shadows/shadow", "shadow")
-      carriable:bring_sprite_to_back(shadow_sprite)
     end
 
     -- Function called when the carriable has fallen.
     local function finish_bounce()
       carriable:clear_collision_tests()
       carriable:stop_movement()
-      carriable:remove_sprite(shadow_sprite)
       set_animation_if_exists("stopped")
       if carriable.on_finish_throw then
         carriable:on_finish_throw() -- Call event
@@ -221,10 +279,11 @@ function carriable_behavior.apply(carriable, properties)
       
       -- Function to compute height for each fall (bounce).
       local function current_height()
+        local progress = t / dur
         if current_bounce == 1 then
-          return h * ((t / dur) ^ 2 - 1)
+          return 2 * h * (progress ^ 2 - progress) - (h * (1.0 - progress))
         end
-        return 4 * h * ((t / dur) ^ 2 - t / dur)
+        return 4 * h * (progress ^ 2 - progress)
       end
 
       -- Start this bounce movement if the previous one ended normally or if the carriable is still moving.
@@ -237,18 +296,23 @@ function carriable_behavior.apply(carriable, properties)
         function movement:on_finished()
           is_bounce_movement_starting = true -- The movement ended without being stopped by an obstacle or from another script.
         end
-        -- Call events and reverse direction on obstacle reached.
+        -- Hit on obstacle reached or sprite collision.
         function movement:on_obstacle_reached()
           local entities = get_overlapping_entities_on_obstacle_reached(movement)
           reverse_direction(slowdown_ratio)
-          for _, entity in pairs(entities) do
-            if entity and entity:is_enabled() then
-              hurt_if_vulnerable(entity)
-              call_hit_events(entity)
-            end
+          if #entities > 0 then
+            hit(entities)
+          else 
+            call_hit_events(nil) -- Call hit events even if the obstacle is not an entity.
           end
-          if #entities == 0 then -- Call hit events even if the obstacle is not an entity.
-            call_hit_events(nil)
+        end
+        function movement:on_position_changed(x, y, layer)
+          local entities = get_overlapping_entities()
+          if #entities > 0 then
+            if is_obstacle_in(entities) then -- Only reverse the move if at least one entity is an obstacle.
+              reverse_direction(slowdown_ratio)
+            end
+            hit(entities)
           end
         end
         is_bounce_movement_starting = false
@@ -310,13 +374,7 @@ function carriable_behavior.apply(carriable, properties)
     carriable:set_can_traverse_ground("low_wall", true)
     carriable:set_can_traverse_ground("prickles", true)
     carriable:set_can_traverse_ground("shallow_water", true)
-    carriable:set_can_traverse("crystal_block", true)
-    carriable:set_can_traverse("stairs", true)
-    carriable:set_can_traverse("stream", true)
-    carriable:set_can_traverse("switch", true)
-    carriable:set_can_traverse("teletransporter", true)
-    carriable:set_can_traverse("destination", true)
-    carriable:set_can_traverse(false)
+    carriable:set_can_traverse(true) -- No way to get traversable entities later, make them all traversable.
 
     -- Set the hero not traversable as soon as possible, to avoid being stuck if the carriable is (re)created on the hero.
     carriable:set_traversable_by("hero", true)
