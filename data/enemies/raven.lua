5634939e5940c6adb24ed014a40e7ac80a433d4c
Vlag <valentin.soudier@gmail.com>
2020-11-04 04:59:58 +0100

    Update Crow and Raven

--- data/enemies/raven.lua
+++ data/enemies/raven.lua
@@ -3,10 +3,11 @@
 -- Raven.
 --
 -- Flying enemy that wait for the hero to be close enough, then take off and attack him.
+-- Possibly set deeply sleeping from the outside to not wake him up when the hero is close, and let wake_up() manually when needed.
 --
--- Methods : enemy:start_attacking()
---           enemy:wake_up()
---           enemy:wait()
+-- Methods : enemy:wake_up()
+--
+-- Properties : is_heavy_sleeper
 --
 ----------------------------------
 
@@ -24,11 +25,11 @@ local is_awake = false
 local turning_time = 0
 
 -- Configuration variables
-local is_deeply_sleeping = enemy:get_property("is_deeply_sleeping") == "true"
+local is_heavy_sleeper = enemy:get_property("is_heavy_sleeper") == "true"
 local after_awake_delay = 1000
 local take_off_duration = 1000
 local targeting_hero_duration = 1000
-local turning_duration = 500
+local turning_away_duration = 500
 local flying_speed = 120
 local flying_height = 24
 local triggering_distance = 60
@@ -50,16 +51,8 @@ local function get_angle_from_sprite(sprite, entity)
   return math.atan2(y - entity_y + sprite_y, entity_x - x - sprite_x)
 end
 
--- Set given angle to movement and correct direction to the enemy sprite.
-local function set_hero_target_angle(movement)
-
-  local angle = get_angle_from_sprite(sprite, hero)
-  movement:set_angle(angle)
-  set_sprite_direction2(angle)
-end
-
--- Start flying to the hero then slightly turn after a delay.
-function enemy:start_attacking()
+-- Start the flying movement.
+local function start_flying()
 
   -- Behavior for each items.
   enemy:set_hero_weapons_reactions(1, {
@@ -69,26 +62,52 @@ function enemy:start_attacking()
     jump_on = "ignored"
   })
 
-  -- Start a target walking from enemy sprite to hero.
-  local movement = enemy:start_straight_walking(0, flying_speed)
+  -- Begin by going to the hero.
+  local angle = get_angle_from_sprite(sprite, hero)
+  local movement = enemy:start_straight_walking(angle, flying_speed)
   movement:set_ignore_obstacles(true)
-  set_hero_target_angle(movement)
-  function movement:on_position_changed()
-    set_hero_target_angle(movement)
-  end
+  set_sprite_direction2(angle)
 
-  -- Replace the on_position() event with the new movement after some time.
-  local turning_angle = (math.random(2) - 1.5) * 0.04 
+  -- Slightly turn to the hero for some time, then turn away from him for some time and finish straight.
+  local is_targeting_hero = true
+  local is_turning_away = false
+  local angle_step = 0.02
+  sol.timer.start(enemy, 10, function()
+    local angle_to_hero = get_angle_from_sprite(sprite, hero)
+    local is_hero_on_left = (angle - angle_to_hero) % circle > math.pi
+    angle = angle + (is_hero_on_left and angle_step or -angle_step)
+    movement:set_angle(angle)
+    set_sprite_direction2(angle)
+    return is_targeting_hero or is_turning_away
+  end)
   sol.timer.start(enemy, targeting_hero_duration, function()
-    function movement:on_position_changed()
-      local angle = movement:get_angle()
-      if turning_time < turning_duration then
-        angle = angle + turning_angle
-        turning_time = turning_time + 10
-        movement:set_angle(angle)
-      end
-      set_sprite_direction2(angle)
+    is_targeting_hero = false
+    if not is_turning_away then
+      angle_step = -angle_step
+      is_turning_away = true
+      return turning_away_duration
     end
+    return
+  end)
+
+  -- Remove the enemy when off screen.
+  function movement:on_position_changed()
+    if not is_targeting_hero and not enemy:is_watched(sprite, true) then
+      enemy:remove()
+    end
+  end
+end
+
+-- Wait for the hero to be close enough and start flying if yes.
+local function wait()
+
+  is_awake = true
+  sol.timer.start(enemy, 100, function()
+    if enemy:get_distance(hero) < triggering_distance then
+      enemy:wake_up()
+      return false
+    end
+    return true
   end)
 end
 
@@ -103,21 +122,8 @@ function enemy:wake_up()
   sprite:set_animation("flying")
   enemy:start_flying(take_off_duration, flying_height, function()
     sol.timer.start(enemy, after_awake_delay, function()
-      enemy:start_attacking()
+      start_flying()
     end)
-  end)
-end
-
--- Wait for the hero to be close enough and start flying if yes.
-function enemy:wait()
-
-  is_awake = true
-  sol.timer.start(enemy, 100, function()
-    if enemy:get_distance(hero) < triggering_distance then
-      enemy:wake_up()
-      return false
-    end
-    return true
   end)
 end
 
@@ -136,7 +142,7 @@ enemy:register_event("on_restarted", function(enemy)
   enemy:set_invincible()
 
   -- States.
-  sprite:set_xy(0, 0)
+  sprite:set_xy(0, is_awake and -flying_height or 0)
   sprite:set_animation("waiting")
   enemy:set_obstacle_behavior("flying")
   enemy:set_can_attack(true)
@@ -145,8 +151,8 @@ enemy:register_event("on_restarted", function(enemy)
 
   -- Wait for hero to be near enough 
   if is_awake then
-    enemy:start_attacking()
-  elseif not is_deeply_sleeping then
-    enemy:wait()
+    start_flying()
+  elseif not is_heavy_sleeper then
+    wait()
   end
 end)
