446ab911baf9b7862f66f515ce4fee2f87903b15
Vlag <valentin.soudier@gmail.com>
2020-11-15 22:55:46 +0100

    Implement custom bombs

--- data/entities/dice.lua
+++ data/entities/dice.lua
@@ -6,12 +6,55 @@
 ----------------------------------
 
 local dice = ...
-local dice_sprite = dice:get_sprite()
 local carriable_behavior = require("entities/lib/carriable")
 carriable_behavior.apply(dice, {})
 
+local map = dice:get_map()
+local sprite = dice:get_sprite()
+
+-- Function to make the carriable not traversable by the hero and vice versa. 
+-- Delay this moment if the hero would get stuck.
+local function set_hero_not_traversable_safely(entity)
+
+  entity:set_traversable_by("hero", true)
+  entity:set_can_traverse("hero", true)
+  if not entity:overlaps(map:get_hero()) then
+    entity:set_traversable_by("hero", false)
+    entity:set_can_traverse("hero", false)
+    return
+  end
+  sol.timer.start(entity, 10, function() -- Retry later.
+    set_hero_not_traversable_safely(entity)
+  end)
+end
+
 -- Set the stopped animation on bounce and randomly change the direction among all available ones.
 dice:register_event("on_bounce", function(dice, num_bounce)
-  dice_sprite:set_animation("stopped")
-  dice:set_direction(math.random(0, dice:get_sprite():get_num_directions()-1))
+
+  sprite:set_animation("stopped")
+  dice:set_direction(math.random(0, dice:get_sprite():get_num_directions() - 1))
+end)
+
+-- Make the hero traversable on thrown to not get stuck.
+dice:register_event("on_thrown", function(dice, direction)
+
+  set_hero_not_traversable_safely(dice)
+end)
+
+-- Setup traversable rules for the dice.
+dice:register_event("on_created", function(dice)
+
+  -- Traversable rules.
+  dice:set_traversable_by(false)
+  dice:set_can_traverse_ground("deep_water", true)
+  dice:set_can_traverse_ground("grass", true)
+  dice:set_can_traverse_ground("hole", true)
+  dice:set_can_traverse_ground("lava", true)
+  dice:set_can_traverse_ground("low_wall", true)
+  dice:set_can_traverse_ground("prickles", true)
+  dice:set_can_traverse_ground("shallow_water", true)
+  dice:set_can_traverse(true) -- No way to get traversable entities later, make them all traversable.
+
+  -- Set the hero not traversable as soon as possible, to avoid being stuck if the carriable is (re)created on the hero.
+  set_hero_not_traversable_safely(dice)
 end)
--- data/entities/explosion.lua
+++ data/entities/explosion.lua
@@ -2,42 +2,86 @@
 --
 -- Explosion allowing some additional properties.
 --
--- Custom properties : strength, hurtable_type_[1 to 10]
+-- Custom properties : damage_on_hero
+--                     explosive_type_[1 to 10]
 -- Events :            on_finished()
 --
 ----------------------------------
 
 local explosion = ...
+local map = explosion:get_map()
 local sprite = explosion:create_sprite("entities/explosion")
 
 -- Configuration variables.
-local strength = tonumber(explosion:get_property("strength")) or 2
-local hurtable_types = {}
+local explosed_entities = {}
+local damage_on_hero = tonumber(explosion:get_property("damage_on_hero")) or 2
+local explosive_types = {}
 for i = 1, 10 do
-  local type = explosion:get_property("hurtable_type_" .. i)
+  local type = explosion:get_property("explosive_type_" .. i)
   if not type then
     break
   end
-  table.insert(hurtable_types, type)
+  table.insert(explosive_types, type)
 end
-if #hurtable_types == 0 then
-  hurtable_types = {"enemy", "hero"}
+if #explosive_types == 0 then
+  explosive_types = {"crystal", "destructible", "door", "enemy", "hero", "sensor"}
 end
 
--- Hurt colliding hero or enemies if needed.
+-- Interact with explosive entities..
 explosion:add_collision_test("sprite", function(explosion, entity)
 
-  for _, type in pairs(hurtable_types) do
-    if entity:get_type() == type then
-      if entity:get_type() == "enemy" then
-        entity:receive_attack_consequence("explosion", entity:get_attack_consequence("explosion"))
-      elseif entity:get_type() == "hero" and not entity:is_invincible()then
-        entity:start_hurt(explosion, strength)
-      end
+  -- Ensure to explode an entity only once by explosion.
+  for _, explosed_entity in pairs(explosed_entities) do 
+    if entity == explosed_entity then
+      return
     end
   end
+  table.insert(explosed_entities, entity)
 
-  -- TODO Cut grass on touched.
+  -- Only try to explode the entity if this explosion can interact with its type.
+  local type
+  for _, explosive_type in pairs(explosive_types) do
+    if entity:get_type() == explosive_type then
+      type = explosive_type
+      break
+    end
+  end
+  if not type then
+    return
+  end
+
+  -- Explode the entity if possible.
+  if type == "crystal" then
+    map:set_crystal_state(not map:get_crystal_state())
+
+  elseif type == "destructible" and entity:get_can_explode() then
+    entity:get_sprite():set_animation("destroy", function()
+      entity:remove()
+    end)
+
+  elseif type == "door" then
+    -- TODO No fucking way to know if the door can be opened with an explosion.
+    --[[if entity:get_can_explode() then
+      entity:open()
+    end--]]
+
+  elseif type == "enemy" then
+    entity:receive_attack_consequence("explosion", entity:get_attack_consequence("explosion"))
+
+  elseif type == "hero" and not entity:is_invincible() and not entity:is_blinking() then
+    entity:start_hurt(explosion, damage_on_hero)
+
+  elseif type == "sensor" then 
+    -- TODO Use another collision mode as sensor has no sprite.
+    --[[if entity.on_collision_explosion then
+      entity:on_collision_explosion()
+    end--]]
+
+  else -- Else can interact with any type of entity if the on_explosion() method is registered.
+    if entity.on_explosion then
+      entity:on_explosion()
+    end
+  end
 end)
 
 -- Explode on created.
