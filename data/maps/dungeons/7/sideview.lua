6df6cfa5871ac6edd07157cf8c2de7e22c5882e8
Vlag <valentin.soudier@gmail.com>
2021-03-15 12:15:27 +0100

    Evil Eagle's sideview

--- data/maps/dungeons/7/sideview.lua
+++ data/maps/dungeons/7/sideview.lua
@@ -1,9 +1,11 @@
 -- Variables
 local map = ...
 local game = map:get_game()
+local is_boss_active = false
 
 -- Include scripts
 local audio_manager = require("scripts/audio_manager")
+local enemy_manager = require("scripts/maps/enemy_manager")
 
 -- Map events
 function map:on_started()
@@ -20,4 +22,27 @@ function map:init_music()
 
   audio_manager:play_music("20_sidescrolling")
 
+end
+
+-- Start the boss
+function sensor_1:on_activated()
+
+  if is_boss_active == false then
+    is_boss_active = true
+    enemy_manager:launch_boss_if_not_dead(map)
+
+    function boss:on_dying()
+      game:start_dialog("maps.dungeons.7.boss_dying")
+    end
+  end
+end
+
+-- Remove the ladder when top reached.
+function sensor_2:on_activated()
+
+  if ladder_bottom:exists() then
+    ladder_background:remove()
+    ladder_top:remove()
+    ladder_bottom:remove()
+  end
 end
