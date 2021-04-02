d13271e9ebd89e4f5c8a80293bd6cc3b024eb6a9
Vlag <valentin.soudier@gmail.com>
2021-04-02 15:57:21 +0200

    Angler Fish

--- data/enemies/boss/projectiles/fry.lua
+++ data/enemies/boss/projectiles/fry.lua
@@ -1,37 +1,82 @@
--- Lua script of enemy boss/angler_fish/angler_fish_fry.
--- This script is executed every time an enemy with this model is created.
+----------------------------------
+--
+-- Angler Fish's Fry.
+--
+-- Swimming enemy for sideview maps used in the Angler Fish fight.
+-- Swim on a sinus curve over the horizontal axis.
+--
+----------------------------------
 
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation for the full specification
--- of types, events and methods:
--- http://www.solarus-games.org/doc/latest
-
+-- Global variables.
 local enemy = ...
+require("enemies/lib/common_actions").learn(enemy)
+
 local game = enemy:get_game()
 local map = enemy:get_map()
 local hero = map:get_hero()
-local sprite
-local movement
+local camera = map:get_camera()
+local sprite = enemy:create_sprite("enemies/boss/angler_fish/fry")
+local quarter = math.pi * 0.5
+local circle = math.pi * 2.0
 
--- Event called when the enemy is initialized.
-function enemy:on_created()
+-- Configuration variables.
+local swimming_speed = 40
+local curve_height = 8
+local curve_duration = 2000
 
-  -- Initialize the properties of your enemy here,
-  -- like the sprite, the life and the damage.
-  sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
+-- Start the enemy movement.
+local function start_swimming()
+
+  -- Horizontal move.
+  local angle = enemy:get_position() > camera:get_position() + camera:get_size() * 0.5 and math.pi or 0 
+  local movement = enemy:start_straight_walking(angle, swimming_speed)
+  movement:set_ignore_obstacles()
+
+  -- Sinus curve on vertical axis.
+  local elapsed_time = 0
+  sol.timer.start(enemy, 10, function()
+    elapsed_time = (elapsed_time + 10) % curve_duration
+    sprite:set_xy(0, math.sin(elapsed_time / curve_duration * circle) * curve_height)
+    return true
+  end)
+
+  function movement:on_position_changed()
+    if not camera:overlaps(enemy:get_bounding_box()) then
+      enemy:start_death()
+    end
+  end
+end
+
+-- Initialization.
+enemy:register_event("on_created", function(enemy)
+
   enemy:set_life(1)
-  enemy:set_damage(1)
-end
+  enemy:set_size(16, 16)
+  enemy:set_origin(8, 13)
+end)
 
--- Event called when the enemy should start or restart its movements.
--- This is called for example after the enemy is created or after
--- it was hurt or immobilized.
-function enemy:on_restarted()
+-- Restart settings.
+enemy:register_event("on_restarted", function(enemy)
 
-  movement = sol.movement.create("target")
-  movement:set_target(hero)
-  movement:set_speed(48)
-  movement:start(enemy)
-end
+  -- Behavior for each items.
+  enemy:set_hero_weapons_reactions({
+  	arrow = 1,
+  	boomerang = 1,
+  	explosion = 1,
+  	sword = 1,
+  	thrown_item = 1,
+  	fire = 1,
+  	jump_on = "ignored",
+  	hammer = 1,
+  	hookshot = 1,
+  	magic_powder = 1,
+  	shield = "protected",
+  	thrust = 1
+  })
+
+  -- States.
+  enemy:set_obstacle_behavior("flying")
+  enemy:set_can_attack(true)
+  enemy:set_damage(2)
+  start_swimming()
+end)
